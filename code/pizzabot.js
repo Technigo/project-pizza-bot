2ab510176ff64e116b3ad9bd18ba5c9febfee037
bealun <53275597+bealun@users.noreply.github.com>
2020-01-30 10:17:19 +0100

    getting started

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,3 +5,12 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 //Put your Javscript code here:
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
+
+let orderName = prompt(`Enter the name of the pizza you want to order today.`)
+
+let orderQuantity = prompt(`How many of ${orderName} do you want?`)
+
+let orderTotal = orderQuantity * pizzaPrice
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`)
