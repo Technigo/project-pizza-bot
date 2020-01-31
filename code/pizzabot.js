e81f3079e7c20f72ca31cac2f0c5b1cb8fbd08e6
Ragna Þorbjörg Úlfarsdóttir <Ragna@Ragnas-MBP.lan>
2020-01-31 13:43:44 +0100

    Half of Iteration 5

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -10,9 +10,27 @@ console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
 alert(`Hey! Happy to serve you a pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
 
 const orderName = prompt(`Enter the name of the pizza you want to order today.`);
+console.log(orderName);
 
-const orderQuantity = prompt(`How many of ${orderName} do you want?`)
+
+// let orderQuantity;
+
+if (orderName == "Vegetarian Pizza") {
+  orderQuantity = Number(prompt(`How many of ${orderName} do you want`))
+} else if (orderName == "Hawaiian Pizza") {
+    orderQuantity = Number(prompt(`How many of ${orderName} do you want`))
+} else if (orderName == "Pepperoni Pizza") {
+    orderQuantity = Number(prompt(`How many of ${orderName} do you want`))
+} else {
+  alert(`Select a pizza from our menu`)
+}
+
+
+console.log(orderQuantity)
+
+
+
+
 
 const orderTotal = (orderQuantity * pizzaPrice);
 alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`);
-
