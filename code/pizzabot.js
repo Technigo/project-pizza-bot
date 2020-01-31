4b642ca9cccebb8e266138eeea2d89fd5114984d
Ragna Þorbjörg Úlfarsdóttir <Ragna@Ragnas-MBP.lan>
2020-01-31 10:53:54 +0100

    3rd Iteration

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -8,3 +8,7 @@ const pizzaPrice = 80
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
 
 alert(`Hey! Happy to serve you a pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
+
+const orderName = prompt(`Enter the name of the pizza you want to order today.`);
+
+const orderQuantity = prompt(`How many of ${orderName} do you want?`)
