2141ec17479ce7bfda609a5395eb94276c2f3b9e
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-01 12:42:32 +0100

    iteration 2

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,4 +4,4 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-console.log(vegetarian, hawaiian, pepperoni, pizzaPrice);
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
