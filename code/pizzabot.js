c00dc19a74352c5c119f3ef395901102c97239a6
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-01 13:00:41 +0100

    iteration 4

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,6 +5,7 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 let orderName
 let orderQuantity
+let orderTotal
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
 
@@ -12,4 +13,6 @@ orderName = prompt("Enter the name of the pizza you want to order today.");
 
 orderQuantity = prompt(`How many of ${orderName} do you want?`);
 
-console.log(orderQuantity);
+orderTotal = pizzaPrice * parseFloat(orderQuantity);
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr.`);
