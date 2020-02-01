2d31ece0c089ab772b4a52fb318b4c5c770398c1
Johanna Åstrand <johanna@Johannas-MacBook-Pro.local>
2020-02-01 12:01:08 +0100

    order function asks for ordername and quantity

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,3 +6,19 @@ const pizzaPrice = 80
 
 //Put your Javscript code here:
 console.log(`${vegetarian}. ${hawaiian}, ${pepperoni}, ${pizzaPrice}`)
+
+const greeting = () => {
+  alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
+
+}
+
+console.log(greeting())
+
+const order = () => {
+  let orderName = prompt("Enter the name of the pizza you want to order today!")
+  let orderQuantity = prompt(`How many of ${orderName} would you like?`)
+}
+
+
+
+order()
