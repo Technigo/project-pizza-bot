fcfe531e56ddc246abb5adfe7dfd57b1eff51ba0
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-01 12:53:52 +0100

    iteration 3

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -3,5 +3,13 @@ const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
+let orderName
+let orderQuantity
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+
+orderName = prompt("Enter the name of the pizza you want to order today.");
+
+orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+console.log(orderQuantity);
