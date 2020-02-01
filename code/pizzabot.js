2246d007e0f85c0d9e49347c14ddf0faae061dad
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-02-01 13:21:13 +0100

    iteration 5

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,6 +6,7 @@ const pizzaPrice = 80
 let orderName
 let orderQuantity
 let orderTotal
+let cookingTime
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
 
@@ -16,3 +17,17 @@ orderQuantity = prompt(`How many of ${orderName} do you want?`);
 orderTotal = pizzaPrice * parseFloat(orderQuantity);
 
 alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr.`);
+
+if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
+  alert("correct");
+} else {
+  alert("Select a pizza from the menu")
+}
+
+if (parseFloat(orderQuantity) <= 2) {
+  alert("The pizzas will take 10 minutes.")
+} else if (parseFloat(orderQuantity) < 6) {
+  alert("The pizzas will take 15 minutes.")
+} else {
+  alert("The pizzas will take 20 minutes.")
+}
