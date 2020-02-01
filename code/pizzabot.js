487a6f47c7b8f7c366760ad0b4d2a549a4ca9c54
Petra Jud <petrajud@Petras-Air-2.lan>
2020-02-01 11:56:45 +0100

    iteration 2

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,7 +5,16 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 //Put your Javscript code here:
+
+// Ieteration 1
 console.log(vegetarian);
 console.log(hawaiian);
 console.log(pepperoni);
 console.log(pizzaPrice);
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+// alert("Hey! Happy to serve your pizza. On our menu we have " + vegetarian + " , " + hawaiian + " and " + pepperoni)
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+// Example : `string text ${variable} string text`
