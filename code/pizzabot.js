b3a9147bae0dff0994da7f980846ae160be27810
Anna T <annat@salmas-MacBook-Air.local>
2020-02-02 14:41:12 +0100

    iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -9,31 +9,56 @@ const pizzaPrice = 80
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and  ${pepperoni}.`);
 
-
 let orderName = prompt("Enter the name of the pizza you want to order today.");
 
+let checkOrderName = (orderName) => {
+  if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
+    prompt(`How many of ${orderName} do you want?`)
+  } else {
+    alert("Please select a pizza from the menu.")
+    //prompt("Enter the name of the pizza you want to order today.")
+  }
+};
 
-if (orderName === vegetarian) {
-  prompt(`How many of ${orderName} do you want?`)
-} else if (orderName === hawaiian) {
-  prompt(`How many of ${orderName} do you want?`)
-} else if (orderName === pepperoni) {
-  prompt(`How many of ${orderName} do you want?`)
-}
-else {
+checkOrderName(orderName)
+
+
+/*if (
+  orderName === vegetarian ||
+  orderName === hawaiian ||
+  orderName === pepperoni
+) {
+  let orderQuantity = prompt(`How many of ${orderName} do you want?`)
+} else {
   alert("Please select a pizza from the menu.")
   prompt("Enter the name of the pizza you want to order today.")
-};
+}
+*/
 
 let orderQuantity = prompt(`How many of ${orderName} do you want?`);
 
-let orderTotal = (orderQuantity * pizzaPrice);
+let totalCost = (orderQuantity) => {
+  (orderQuantity * pizzaPrice);
+}
 
-if (orderQuantity < 3) {
+//totalCost(orderQuantity)
+
+//let orderTotal = (orderQuantity * pizzaPrice);
+
+/*if (orderQuantity < 3) {
   alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take 10 minutes`)
 } else if (orderQuantity < 6) {
   alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take 15 minutes`)
 } else {
   alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take 20 minutes`)
+}; */
+
+
+
+let cookingTime = (orderQuantity) => {
+  if (orderQuantity < 3) { alert(`Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(orderQuantity)} kr. The pizzas will take 10 minutes`) }
+  else if (orderQuantity < 6) { alert(`Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(orderQuantity)} kr. The pizzas will take 15 minutes`) }
+  else { alert(`Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(orderQuantity)} kr. The pizzas will take 20 minutes`) }
 };
 
+cookingTime(orderQuantity)
