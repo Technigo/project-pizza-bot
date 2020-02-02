e77e5921586cf4cee49d9868620fe2f44e016f94
Louise Eriksson <louise@Louises-Air.lan>
2020-02-02 11:20:57 +0100

    iteration 1 finished

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,4 +4,8 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
+console.log(vegetarian)
+console.log(hawaiian)
+console.log(pepperoni)
+console.log(pizzaPrice)
 //Put your Javscript code here:
