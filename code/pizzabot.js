f1582d6f1d0f608aec2c596addee5c1bbcf535e1
Christina Persson <christina@Christinas-MacBook.local>
2020-02-02 13:33:18 +0100

    connected js and style to html

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,12 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
+
+console.log(vegetarian);
+console.log(hawaiian);
+console.log(pepperoni);
+console.log(pizzaPrice);
 
 //Put your Javscript code here:
