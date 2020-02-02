726c1809258fe0440bc8ad2806219b47d43b0eb4
Lisas Sturegard <lisasturegard@hotmail.com>
2020-02-02 09:29:44 +0100

    Third interation Pizza Project

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -10,8 +10,19 @@ var x = vegetarian
 var y = hawaiian
 var z = pepperoni
 
-
 alert("Hey! Happy to serve you pizza! On our menu we have " + x + ", " + y + " and " + z + ".")
+
+
+
+
+let orderName = prompt('Enter the name of the pizza you want to order today.');
+console.log(orderName);
+
+let orderQuantity = prompt('How many of ' + orderName + ' do you want?');
+console.log(orderQuantity);
+
+
+
 
 
 
