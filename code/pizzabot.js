90dd2143b4ce95fafe6cf7b597fd5852e6856509
Johanna Olsen <johanna.olsen.92@gmail.com>
2020-02-02 13:14:36 +0100

    Iteration 2 - Greet the customer

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,23 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const X = "Vegetarian Pizza"
+const Y = "Hawaiian Pizza"
+const Z = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
+
 //Put your Javscript code here:
+
+console.log(X, Y, Z, pizzaPrice)
+
+/*
+## **Iteration 2 - Greet the customer**
+
+In code/pizzabot.js, you can start coding the bot interaction. The first step is to greet a new customer.
+Use `alert()` to print the message `"Hey! Happy to serve your pizza. On our menu we have X, Y and Z"`.
+Use the variables `vegetarian`, `hawaiian` and `pepperoni` to replace `X`, `Y` and `Z` in the message so that it reads
+`"Hey! Happy to serve your pizza. On our menu we have Vegetarian Pizza, Hawaiian Pizza and Pepperoni Pizza"`
+
+*/
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${X}, ${Y} and ${Z}`)
+
