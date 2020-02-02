62e5b69c4e0e81afaa8b8faffe79cd6afbefb667
RUTAN47 <johan.rutberg@gmail.com>
2020-02-02 14:58:13 +0100

    ration 4 - Fin

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -15,3 +15,7 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 let orderName = prompt('Enter the name of the pizza you want to order today.');
 
 let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+let orderTotal = (orderQuantity * pizzaPrice);
+
+alert(`Great, I'll get started on your ${orderName}s right away, it will cost ${orderTotal}kr.`);
