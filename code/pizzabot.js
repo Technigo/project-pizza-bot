2b678b0eb340b4eb767cd7fad39de0535b2ff4b7
RUTAN47 <johan.rutberg@gmail.com>
2020-02-02 14:54:09 +0100

    ## **Iteration 3 - Ask the customer**

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -11,3 +11,7 @@ const pizzaPrice = 80
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice);
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`)
+
+let orderName = prompt('Enter the name of the pizza you want to order today.');
+
+let orderQuantity = prompt(`How many of ${orderName} do you want?`);
