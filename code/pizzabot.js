bf7a3d94a2134e57caef0b9c84a48e0a172c1540
Christina Persson <christina@Christinas-MacBook.local>
2020-02-03 14:03:12 +0100

    Done with base goals

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -36,29 +36,33 @@ const cookingTime = orderQuantity => {
   return cookingTime;
 };
 
-/*const clickFunction = () => {
-  let orderName = document.getElementById("order-type").value;
+document.getElementById(
+  "menu"
+).innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`;
+
+const clickFunction = () => {
+  //Fungerar toppen!
+
+  let orderName = prompt(
+    "Enter the name of the pizza you want to order today."
+  );
+
+  if (checkOrderName(orderName)) {
+    let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+    console.log(orderQuantity);
+
+    document.getElementById(
+      "final-message"
+    ).innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(
+      orderQuantity
+    )} kr. The pizzas will take ${cookingTime(orderQuantity)} minutes.`;
+  } else {
+    document.getElementById("final-message").innerHTML =
+      "Select a pizza from the menu";
+  }
+};
+/*
+ let orderName = document.getElementById("order-type").value;
   console.log(orderName);
   console.log("kommer vi hit?");
-};*/
-
-//Fungerar toppen!
-alert(
-  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
-);
-
-let orderName = prompt("Enter the name of the pizza you want to order today.");
-
-if (checkOrderName(orderName)) {
-  let orderQuantity = prompt(`How many of ${orderName} do you want?`);
-  console.log(orderQuantity);
-
-  document.getElementById(
-    "final-message"
-  ).innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(
-    orderQuantity
-  )} kr. The pizzas will take ${cookingTime(orderQuantity)} minutes.`;
-} else {
-  document.getElementById("final-message").innerHTML =
-    "Select a pizza from the menu";
-}
+*/
