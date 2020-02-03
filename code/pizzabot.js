b214f4911925874bd6123ecb3cc2058d8c4734aa
elsareutersward <elsa.reutersward@gmail.com>
2020-02-03 12:43:03 +0100

    Iteration 3

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -9,3 +9,7 @@ console.log("Vegetarian Pizza", "Hawaiian Pizza", "Pepperoni Pizza", 80)
 //Put your Javscript code here:
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian} , ${hawaiian} and ${pepperoni}`)
+
+
+ const orderName = prompt(`Enter the name of the pizza you want to order today.`)
+ const orderQuantity = prompt(`How many of ${orderName} do you want?`)
