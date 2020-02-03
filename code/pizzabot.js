ea062f16141e88004a223c9ba012c7271a07fac4
Anne-Sophie Gendron <email@example.comannesophie.gendron@gmail.com>
2020-02-03 22:08:09 +0100

    iteration 4

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,8 +1,8 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
 
 
 // Iteration 2: Greet the customer with alert()
@@ -12,6 +12,11 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 const orderName = prompt("Enter the name of the pizza you want to order today.");
 
-const orderQuantity = prompt("How many of " + orderName + " do you want?");
+const orderQuantity = prompt(`How many of ${orderName}  do you want?`);
 
+// Iteration 4 - Finalize the order
+
+const orderTotal = (orderQuantity * pizzaPrice);
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal}  kr`)
 
