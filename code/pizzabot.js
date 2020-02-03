e1e64ad958bf6d5c7d8a241e3447f4af08b64840
Louise Eriksson <louise@Louises-MacBook-Air.local>
2020-02-03 18:00:47 +0100

    tried to fix how to use the answer from prompt to orderQuantity

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -10,6 +10,7 @@ console.log(pepperoni)
 console.log(pizzaPrice)
 
 
+
 let message;
 message = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, ${pepperoni}.`
 
@@ -19,10 +20,22 @@ var orderName = prompt(`Enter the name of the pizza you want to order today?`)
 
 let orderQuantity = prompt(`How many of ${orderName} do you want?`)
 
+if (orderName === vegetarian) {
+  prompt(`How many of ${orderName} do you want?`)
+} else if (orderName === hawaiian) {
+  prompt(`How many of ${orderName} do you want?`)
+} else if (orderName === pepperoni) {
+  prompt(`How many of ${orderName} do you want?`)
+} else {
+  alert(`Please select something from our menu`)
+}
+
+
+
+
+
 let total = (pizzaPrice * orderQuantity)
-
-var orderTotal = prompt(`Great, we will get started on your ${orderName} right away, it will cost ${total} kr`)
-
+var orderTotal = prompt(`Great, we will get started on your ${orderName} right away, it will cost ${total} kr, the pizzas will take ${orderQuantity}`)
 
 
 
@@ -31,4 +44,6 @@ var orderTotal = prompt(`Great, we will get started on your ${orderName} right a
 
 
 
-
+if (orderQuantity >= 1 && orderQuantity <= 2) {
+  orderQuantity = prompt(`The pizzas will take 10 minutes`)
+}
