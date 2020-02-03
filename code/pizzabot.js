4dfbde7cac695c16fc4b17b18cf1e048b77a6f38
Lisas Sturegard <lisasturegard@hotmail.com>
2020-02-03 13:28:15 +0100

    Iteration 4 Project Pizza

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -12,17 +12,39 @@ var z = pepperoni
 
 alert("Hey! Happy to serve you pizza! On our menu we have " + x + ", " + y + " and " + z + ".")
 
-
-
-
-let orderName = prompt('Enter the name of the pizza you want to order today.');
+let orderName = prompt("Enter the name of the pizza you want to order today.");
 console.log(orderName);
 
-let orderQuantity = prompt('How many of ' + orderName + ' do you want?');
+if (orderName == "vegetarian") {
+  modifiedName = x;
+} else if (orderName == "hawaiian") {
+  modifiedName = y;
+} else if (orderName == "pepperoni") {
+  modifiedName = z;
+} else {
+  alert('try again')
+}
+
+let orderQuantity = prompt("How many of " + modifiedName + " do you want?");
 console.log(orderQuantity);
 
+const orderTotal = orderQuantity * 80;
+
+console.log(`The customer has ordered ${orderQuantity} of ${orderName}.`);
+alert("Great, I'll get started on your " + modifiedName + " right away, it will cost you " + orderTotal + "kr.");
+
+console.log("It will cost " + orderQuantity * 80 + "kr.");
+
+
+
+
+
+
+
+/*
 let orderTotal = orderQuantity * pizzaPrice
-alert("Great, I'll get started on your " + orderName + " right away, it will cost " + orderTotal + " kr.")
+alert("Great, I'll get started on your " + orderName + " right away, it will cost " + orderTotal + " kr.");*/
+
 
 
 
