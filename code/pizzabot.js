32c579072d4c8888742fed9cb69f240a322f9b04
Emma Prené <emma.prene@gmail.com>
2020-02-03 09:27:43 +0100

    Iteration 4

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -11,3 +11,7 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 const orderName = prompt(`Enter the name of the pizza you want to order today.`)
 const orderQuantity = prompt(`How many of ${orderName} do you want?`)
+
+const orderTotal = orderQuantity * pizzaPrice
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`)
