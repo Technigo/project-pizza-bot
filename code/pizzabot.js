a210153a483c08d3fba0166a362598ee447d6344
Petra Jud <petrajud@Petras-Air-2.lan>
2020-02-03 23:32:10 +0100

    Iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,6 @@
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
-
 const pizzaPrice = 80
 
 //Put your Javscript code here:
@@ -12,14 +11,14 @@ console.log(hawaiian);
 console.log(pepperoni);
 console.log(pizzaPrice);
 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+//Iteration 2
+//alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
 
 // alert("Hey! Happy to serve your pizza. On our menu we have " + vegetarian + " , " + hawaiian + " and " + pepperoni)
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 // Example : `string text ${variable} string text`
 
-
+// Iteration 3
 //let orderName = 
 // Check if user input is stored in orderName
 //console.log(orderName);
@@ -29,45 +28,81 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 
 // Iteration 4
-
-
 //alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`);
-
 // https://www.w3schools.com/js/js_arithmetic.asp
 
 //Iteration 5 - conditionals
-
-// const cookingTime = () => {
-  
+// const orderWhole = () => {
+//   let orderName = prompt(`Enter the name of the pizza you want to order today.`);
+//   if ((orderName === vegetarian) || (orderName === hawaiian) || (orderName === pepperoni)) {
+//   let orderQuantity = prompt(`Super! How many ${orderName} would you like?`);
+//   let orderTotal = orderQuantity * pizzaPrice;
+//     if (orderQuantity >0 && orderQuantity <=2) {
+//     cookingTime = "10 minutes"
+//   }
+//     else if (orderQuantity >2 && orderQuantity <6) {
+//     cookingTime = "15 minutes"
+//   }
+//     else if (orderQuantity >=6) {
+//     cookingTime = "20 minutes"
+//   }
+//     else {
+//     alert("Sorry, that option does not exist. Please start over!");
+//     orderWhole();
+//     console.log("order quantity invalid")
+//     };
+//   alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and your order will be ready in ${cookingTime}`);
+//   console.log("works")
 // }
+// else {
+//   alert("Uhoh, we do not have that on the menu.");
+//   orderWhole();
+//   console.log("doesn't work")
+// }
+// }
+// orderWhole()
 
-const orderWhole = () => {
-  let orderName = prompt(`Enter the name of the pizza you want to order today.`);
-  if ((orderName === vegetarian) || (orderName === hawaiian) || (orderName === pepperoni)) {
-  let orderQuantity = prompt(`Super! How many ${orderName} would you like?`);
-  let orderTotal = orderQuantity * pizzaPrice;
-    if (orderQuantity >0 && orderQuantity <=2) {
-    cookingTime = "10 minutes"
-  }
-    else if (orderQuantity >2 && orderQuantity <6) {
-    cookingTime = "15 minutes"
-  }
-    else if (orderQuantity >=6) {
-    cookingTime = "20 minutes"
-  }
-    else {
-    alert("Sorry, that option does not exist. Please start over!");
-    orderWhole();
-    console.log("order quantity invalid")
-    };
-  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and your order will be ready in ${cookingTime}`);
-  console.log("works")
-}
-else {
-  alert("Uhoh, we do not have that on the menu.");
-  orderWhole();
-  console.log("doesn't work")
-}
-}
-orderWhole()
+// Iteration 6
+// Inform user about the menu
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
 
+// Store users input in orderName
+orderName = prompt(`Enter the name of the pizza you want to order today.`);
+
+const checkOrderName = () => {
+  if (orderName == vegetarian || orderName == hawaiian || orderName == pepperoni) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Invoke function with users input as parameter
+checkOrderName(orderName);
+
+const totalCost = (orderQuantity) => {
+  alert(`That will be a total of ${orderQuantity * pizzaPrice}kr`);
+  return orderQuantity * pizzaPrice;
+}
+
+const cookingTime = (orderQuantity) => {
+  if (orderQuantity >0 && orderQuantity <=2) {
+    alert("This order will take 10 minutes")
+  } else if ( orderQuantity >2 && orderQuantity <6) {
+    alert("This order will take 15 minutes")
+  } else if ( orderQuantity >=6) {
+    alert("This order will take 20 minutes")
+  } else {
+    alert(`Sorry you can´t order ${orderQuantity} pizzas!`)
+  }
+}
+
+const orderUp = () => {
+  if ( checkOrderName() ) {
+    orderQuantity = prompt(`How many of ${orderName} do you want?`)
+    totalCost(orderQuantity);
+    cookingTime(orderQuantity);
+  }
+}
+
+orderUp();
