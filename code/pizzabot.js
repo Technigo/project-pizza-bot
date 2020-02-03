a64e24127f320f8e81f6888a59e3534abf090eb4
elsareutersward <elsa.reutersward@gmail.com>
2020-02-03 13:59:01 +0100

    Iteration 5

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -8,11 +8,29 @@ console.log("Vegetarian Pizza", "Hawaiian Pizza", "Pepperoni Pizza", 80)
 
 //Put your Javscript code here:
 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian} , ${hawaiian} and ${pepperoni}`)
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
+
+const orderName = prompt(`Enter the name of the pizza you want to order today.`)
+ let orderQuantity
+    if ((orderName == "Vegetarian Pizza" || orderName == "Hawaiian Pizza" || orderName == "Pepperoni Pizza")) {
+        orderQuantity = Number(prompt(`How many of ${orderName} do you want?`))
+    } else {
+        alert(`Select a pizza from the menu`)
+    
+    }
+
+const orderTotal = orderQuantity * pizzaPrice
+
+let orderTime
+    if (orderQuantity <= 2) {
+        orderTime = 10
+    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+        orderTime = 15
+    } else {
+        orderTime = 20
+    }
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza will take ${orderTime} minutes.`)
+ 
 
 
- const orderName = prompt(`Enter the name of the pizza you want to order today.`)
- const orderQuantity = prompt(`How many of ${orderName} do you want?`)
-
- const orderTotal = orderQuantity * pizzaPrice
- alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`)
