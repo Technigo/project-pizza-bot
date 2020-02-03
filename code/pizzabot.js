65306289d3ed7b6b45ec0dfe445bacc84a72a001
Ragna Þorbjörg Úlfarsdóttir <Ragna@Ragnas-MBP.lan>
2020-02-03 15:45:13 +0100

    Iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -9,31 +9,69 @@ console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
 
 alert(`Hey! Happy to serve you a pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
 
-const orderName = prompt(`Enter the name of the pizza you want to order today.`);
-console.log(orderName);
+// const checkOrderName = () => {
+  
+// }
 
+const orderName = prompt(`Enter the name of the pizza you want to order today.`);
 
 let orderQuantity;
 
-if (orderName == "Vegetarian Pizza") {
-  orderQuantity = Number(prompt(`How many of ${orderName} do you want`))
-} else if (orderName == "Hawaiian Pizza") {
-    orderQuantity = Number(prompt(`How many of ${orderName} do you want`))
-} else if (orderName == "Pepperoni Pizza") {
-    orderQuantity = Number(prompt(`How many of ${orderName} do you want`))
-} else {
-  alert(`Select a pizza from our menu`)
+const checkOrderName = () => {
+  if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
+    orderQuantity = Number(prompt(`How many of ${orderName} would you like?`));
+    return true;
+    } else {
+    alert(`Your choice, ${orderName}, is not on our menu. Please choose again`);
+    return false;
+    
+  };
+}
+
+checkOrderName();
+
+
+console.log(orderName);
+
+
+
+
+let orderTotal;
+
+const totalCost = () => {
+  orderTotal = orderQuantity * pizzaPrice;
+  console.log(orderTotal);
+
+};
+
+totalCost();
+
+let ovenTime;
+
+const cookingTime = () => {
+  if (orderQuantity < 3) {
+    ovenTime = 10;
+    return(+10);
+  } else if (orderQuantity > 5) {
+    ovenTime = 20;
+    return(+20);
+  } else {
+    ovenTime = 15;
+    return(+15);
+  };
+
 }
 
 
-console.log(orderQuantity)
-const orderTotal = (orderQuantity * pizzaPrice);
+cookingTime();
 
-if (orderQuantity < 3) {
-  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take 10 minutes.`)
-} else if (orderQuantity > 5) {
-  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take 20 minutes.`)
-} else {
-  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take 15 minutes.`)
-}
+console.log(ovenTime);
+  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take ${ovenTime} minutes`);
 
+
+console.log(orderQuantity);
+
+
+
+
+// 
