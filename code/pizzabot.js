a1c17ded38cde3b4e9adf449bf4307b3d48dcd32
Anne-Sophie Gendron <email@example.comannesophie.gendron@gmail.com>
2020-02-05 22:38:11 +0100

    button question

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -7,9 +7,9 @@ const pizzaPrice = 80;
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice);
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
-
 let orderName = prompt("Enter the name of the pizza you want to order today.");
 console.log(orderName);
+
 let orderQuantity;
 
 const checkOrderName = () => {
