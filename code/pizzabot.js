8047bea128c0d04099ebd8b40936271a770fb43f
VickBe <32632292+VickBe@users.noreply.github.com>
2020-02-05 15:18:39 +0100

    Cleaned up the code.Ready for delivery

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -3,51 +3,48 @@ const hawaiian = "Hawaiian";
 const pepperoni = "Pepperoni";
 const pizzaPrice = 80;
 
-//Put your Javscript code here:
-
+const greet = (`Hey! Happy to serve your pizza! <br> On our menue we have <span style="color:#fa3e54">${vegetarian}</span>, <span style="color:#4c3ffc">${hawaiian}</span> and <span style="color:#059e7b">${pepperoni}</span> pizzas.`);
+document.getElementById("welcome").innerHTML = greet;
 
 let clicked = () => {
-  const greet = alert(`Hey! Happy to serve your pizza. On our menue we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
-}
 
+  let checkOrderName = () => {
+    let orderName = prompt('Enter the name of the pizza you want to order today');
 
-let checkOrderName = () => {
-  let orderName = prompt('Enter the name of the pizza you want to order today');
-
-  if (orderName == vegetarian || orderName == hawaiian || orderName == pepperoni) {
-    orderQuantity = Number(prompt(`How many ${orderName} would you like?`));
-    return true;
-  } else {
-    alert('Select a pizza from the menue')
-    return false;
+    if (orderName == vegetarian || orderName == hawaiian || orderName == pepperoni) {
+      orderQuantity = Number(prompt(`How many ${orderName} would you like?`));
+      return true;
+    } else {
+      alert('Select a pizza from the menue')
+      return false;
+    }
+    // return orderQuantity
   }
-  return orderQuantity
-}
-checkOrderName()
+  checkOrderName()
 
+  // Hur accessa orderName in i funktionen:
 
-let totalCost = () => {
-
-  let pizzaCalculation = pizzaPrice * orderQuantity
-  const pizzaKronor = (`This order will cost you ${pizzaCalculation} kronor`)
-  document.getElementById("pizzapris").innerHTML = pizzaKronor;
-}
-
-totalCost()
-
-
-let cookingTime = () => {
-
-  if (orderQuantity <= 2) {
-    const pizzaTime10 = (`The pizzas will take 10 min`)
-    document.getElementById("pizzaleverans").innerHTML = pizzaTime10;
-  }else if (orderQuantity >= 6) {
-    const pizzaTime20 = (`The pizzas will take 20 min`)
-    document.getElementById("pizzaleverans").innerHTML = pizzaTime20;
-  }else {
-    const pizzaTime15 = (`The pizzas will take 15 min`)
-    document.getElementById("pizzaleverans").innerHTML = pizzaTime15;
+  let totalCost = () => {
+    let pizzaCalculation = pizzaPrice * orderQuantity
+    let pizzaCost = (`This order will cost you ${pizzaCalculation} kronor.`)
+    document.getElementById("pizzaCost").innerHTML = pizzaCost;
   }
-}
 
-cookingTime()
+  totalCost()
+
+
+  let cookingTime = () => {
+
+    if (orderQuantity <= 2) {
+      const pizzaTime10 = (`The pizzas will take 10 min.`)
+      document.getElementById("pizzaDelivery").innerHTML = pizzaTime10;
+    } else if (orderQuantity >= 6) {
+      const pizzaTime20 = (`The pizzas will take 20 min.`)
+      document.getElementById("pizzaDelivery").innerHTML = pizzaTime20;
+    } else {
+      const pizzaTime15 = (`The pizzas will take 15 min.`)
+      document.getElementById("pizzaDelivery").innerHTML = pizzaTime15;
+    }
+  }
+  cookingTime()
+}
