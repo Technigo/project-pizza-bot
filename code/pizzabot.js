d598209d950cfcdda4b0ffe005dbc87f8d4d2abd
elsareutersward <elsa.reutersward@gmail.com>
2020-02-05 14:45:59 +0100

    Iteration 7 - Final

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -8,45 +8,54 @@ console.log("Vegetarian Pizza", "Hawaiian Pizza", "Pepperoni Pizza", 80)
 
 //Put your Javscript code here:
 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
-const orderName = prompt(`Enter the name of the pizza you want to order today.`)
-
-let orderQuantity 
+document.getElementById("button").onclick = function() {myFunction()};
+function myFunction() {
+    alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
+    const orderName = prompt(`Enter the name of the pizza you want to order today.`)
+    checkOrderName(orderName);
+  }
 
 const checkOrderName = (orderName) => { 
     const isCorrect = orderName == "Vegetarian Pizza" || orderName == "Hawaiian Pizza" || orderName == "Pepperoni Pizza"
-    
+    let orderQuantity
   
     if (isCorrect) {
         orderQuantity = Number(prompt(`How many of ${orderName} do you want?`))
-        
+
+        const totalCost = (orderQuantity) => {
+        return orderQuantity * pizzaPrice
+        }
+
+         const cookingTime = (orderQuantity) => {
+            if (orderQuantity <= 2) {
+            return 10
+            } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+            return 15
+            } else {
+            return 20
+            }  
+        }
+    
+        document.getElementById("final-greeting").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(orderQuantity)} kr. The pizza will take ${cookingTime(orderQuantity)} minutes.`
+
     } else {
         alert(`Select a pizza from the menu`)
     }
-    
   }
   
-  checkOrderName(orderName)
+ 
 
-  const totalCost = (orderQuantity) => {
-    return orderQuantity * pizzaPrice
-  }
+  
 
 
+  
 
 
-let orderTime
-const cookingTime = (orderQuantity) => {
-    if (orderQuantity <= 2) {
-        return 10
-    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-        return 15
-    } else {
-        return 20
-    }
-}
 
-alert(`Great, I'll get started on your ${orderName} right away, it will cost ${totalCost(orderQuantity)} kr. The pizza will take ${cookingTime(orderQuantity)} minutes.`)
+
+
+
+
  
 
 
