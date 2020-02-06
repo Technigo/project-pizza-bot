02da093a952c30e8c1bf6bea985040cad43a68f0
Anne-Sophie Gendron <email@example.comannesophie.gendron@gmail.com>
2020-02-06 22:01:09 +0100

    final version

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,44 +6,44 @@ const pizzaPrice = 80;
 
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice);
 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
-let orderName = prompt("Enter the name of the pizza you want to order today.");
-console.log(orderName);
-
+let orderName;
 let orderQuantity;
 
-const checkOrderName = () => {
-  if (orderName === 'vegetarian' || orderName === 'hawaiian' || orderName === 'pepperoni') {
+const startOrder = () => {
+  alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+  orderName = prompt("Enter the name of the pizza you want to order today.");
+  console.log(orderName);
+  checkOrderName(orderName)
+};
+
+const checkOrderName = (orderName) => {
+  if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
     orderQuantity = prompt(`How many of ${orderName} pizza do you want?`);
     console.log(orderQuantity);
-    return true;
+    showConfirmation()
   } else {
     alert("This pizza is not on the menu. Please try again.");
     orderName = prompt("Enter the name of the pizza you want to order today.");
     checkOrderName()
-  };
+  }
 };
-checkOrderName();
-
-let orderTotal;
 
 const totalCost = () => {
-  orderTotal = orderQuantity * pizzaPrice;
+  return orderQuantity * pizzaPrice
 };
-console.log(orderTotal);
-totalCost();
-
-let cookingTime;
 
 const getCookingTime = () => {
   if (orderQuantity < 3) {
-    cookingTime = '10';
-  } else if (orderQuantity >= 3 && orderTotal <= 5) {
-    cookingTime = '15';
+    return '10';
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    return '15';
   } else {
-    cookingTime = '20';
+    return '20';
   }
 };
-getCookingTime();
 
-document.getElementById("order-confirmation").innerHTML = (`Great! I'll get started on your ${orderName} pizza right away, it will cost ${orderTotal} kr. The pizzas will take ${cookingTime} minutes!`);
+const showConfirmation = () => {
+  document.getElementById("order-confirmation").innerHTML = (`Great! I'll get started on your ${orderName} pizza right away, it will cost ${totalCost()} kr. The pizzas will take ${getCookingTime()} minutes!`);
+};
+
+document.getElementById("orderButton").onclick = startOrder;
