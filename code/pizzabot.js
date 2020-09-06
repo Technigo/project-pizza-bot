11dff3b5093a284b675967af18bcfbb2b5faf7ad
Jamie Cook <jamie.p.cook@outlook.com>
2020-09-06 16:44:39 +0200

    Iteration 1-4 completed

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,4 +4,33 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
+//console.log(`${vegetarian} + ${hawaiian} + ${pepperoni} + ${pizzaPrice}`);
+
 //Put your Javscript code here:
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+let placeOrder = () => {
+    let orderName = prompt('Enter the name of the pizza you want to order today');
+    let orderQuantity = prompt(`How many of ${orderName} would you like?`); 
+
+    let orderTotal = (orderQuantity * pizzaPrice);
+    alert(`Great I'll get started on your ${orderName} right away. It will cost ${orderTotal}kr`);
+
+}
+placeOrder();
+/*
+let checkOrder = () => {
+    if (orderName === vegetarian || hawaiian || pepperoni) {
+        prompt("Next prompt") 
+        } else { 
+            alert(`Select a pizza from the menu`);
+        }
+}
+checkOrder();
+
+if (orderName !== ${vegetarian} || ${hawaiian} || ${pepperoni}) {
+
+    } else { alert(`Select a pizza from the menu`);
+
+ */
+
