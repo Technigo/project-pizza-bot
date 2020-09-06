a1a25ee2dc880c5b450829b8f52f0e1d4a82626c
Marjaana Tienaho <marjaanatienaho@Marjaanas-Air.lan>
2020-09-06 08:59:17 +0200

    Added code for iteration 2 and 3

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,3 +5,24 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 //Put your Javscript code here:
+console.log(vegetarian)
+console.log(hawaiian)
+console.log(pepperoni)
+
+//Greet the customer (Iteration 2)
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
+
+//Ask the customer (Iteration 3)
+const orderName = prompt("Enter the name of the pizza you want to order today: ")
+console.log(orderName)
+const orderQuantity = prompt(`How many of ${orderName} do you want?`)
+console.log(orderQuantity)
+//Finalize the order (Iteration 4)
+
+//Add conditonals (Iteration 5)
+
+//Add functions (Iteration 6)
+
+//Make it visual (Iteration 7)
+
+
