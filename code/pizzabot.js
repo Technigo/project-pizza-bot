8cd64156b36aa57a40c4493f30516c948d7fe360
Corneliaryas <hello@corneliaryas.com>
2020-09-07 14:45:25 +0200

    Iteration 3

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -12,3 +12,7 @@ console.log(pepperoni);
 
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+const orderName = prompt("Enter the name of the pizza you want to order today.");
+
+const orderQuantity = prompt(`How many of ${orderName} do you want?`);
