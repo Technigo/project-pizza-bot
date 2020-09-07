b22d50de5b6604496afb5eca0b3efed61f91bad0
Corneliaryas <hello@corneliaryas.com>
2020-09-07 15:58:50 +0200

    Iteration 5

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -15,8 +15,32 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 const orderName = prompt("Enter the name of the pizza you want to order today.");
 
-const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+let orderQuantity ;
+
+if (orderName === vegetarian) {
+    orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+} else if (orderName === hawaiian) {
+    orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
+} else if (orderName === pepperoni) {
+    orderQuantity = prompt(`How many of ${orderName} do you want?`);
+} else {
+    alert("Select a pizza from the menu"); 
+    orderName = prompt("Enter the name of the pizza you want to order today.");
+};
+
+let cookingTime; 
+
+if (orderQuantity <= 2) {
+    cookingTime = 10;
+} else if (orderQuantity > 2 && orderQuantity < 6) {
+    cookingTime = 15;
+} else {
+    cookingTime = 20;
+};
 
 const orderTotal = orderQuantity * pizzaPrice;
-alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal}`);  
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal}kr. The pizza(s) will take ${cookingTime} minutes.`);
 
