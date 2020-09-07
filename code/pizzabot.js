1f427a4a2c6bf32db53c684b3781e19ddb4aeac6
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2020-09-07 09:31:33 +0200

    linked js file with html file and checked variables are correct

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,14 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
+
+// Checking variables with console.log
+console.log(vegetarian);
+console.log(hawaiian);
+console.log(pepperoni);
+
+console.log(pizzaPrice);
 
 //Put your Javscript code here:
