d6a0c39e366a8a3a8fc252d2dff284f63002df3f
AxelHammarback <axel.hammarback@gmail.com>
2020-09-07 15:00:03 +0200

    Iteration 3 questons to the customer

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -12,3 +12,10 @@ console.log(pizzaPrice)
 
 // Use alert() to print the message "Hey! Happy to serve your pizza. On our menu we have X, Y and Z".
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
+
+const orderName = prompt(`Enter the name of the pizza you want to order today`)
+console.log(orderName)
+
+const orderQuantity= prompt(`How many of ${orderName} do you want?`)
+console.log(orderQuantity)
+
