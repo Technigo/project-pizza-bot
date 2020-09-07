b6a5101a41e754e936f64745aad39eb19d750b5d
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-09-07 13:51:09 +0200

    Iteration 2

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,3 +6,5 @@ const pizzaPrice = 80
 
 //Put your Javscript code here:
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
+
+alert(`Hello! Happy to serve you pizza. On our menu we have ${vegetarian}, ${pepperoni}, ${hawaiian}`);
