72755f19879f711c84b9faa045a4794e2f82b043
emma-tech <emma.urman@gmail.com>
2020-09-07 14:20:27 +0200

    Iteration 3 done

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,3 +6,8 @@ const pizzaPrice = 80
 
 //Put your Javscript code here:
 alert(`Hey, happy to serve you pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+let orderName = prompt('Enter the name of the pizza you want to order today.');
+
+let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+
