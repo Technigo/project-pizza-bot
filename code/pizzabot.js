add5ce3c64ec651d8570198be698eb46856712d1
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-07 16:33:43 +0200

    Updated the javascript file with the first 4 iterations and one conditional

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,3 +5,27 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 //Put your Javscript code here:
+
+// ITERATION 2
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${hawaiian}, ${vegetarian} and ${pepperoni}`);
+
+// ITERATION 3
+
+const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+const orderName = prompt('Enter the name of the pizza you want to order today.');
+if (orderName === orderName) {
+    const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+}
+else alert("Sorry order something from our menu");
+
+
+// ITERATION 4
+
+const orderTotal = (orderQuantity * pizzaPrice);
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`);
+
+
+
+
