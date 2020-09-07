0815fda0937429c528e38470f494372202dc76dd
MindstormingAB <s.elander@mindstorming.se>
2020-09-07 16:41:11 +0200

    Forth Itteration - Finalise the Order

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -17,5 +17,9 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 const orderName = prompt('Enter the name of the pizza you want to order today.')
 console.log(orderName)
 
-const orderQuantity = prompt(`How many of ${orderName} do you want?`)
+const orderQuantity = prompt(`How many of ${orderName} pizza(s) do you want?`)
 console.log(orderQuantity)
+
+const orderTotal = orderQuantity * pizzaPrice
+
+alert(`Great, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr.`)
