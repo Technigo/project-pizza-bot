dcd3f89d297783498f133a7ef29604c1b057546b
LINDA HINTZE <lindahintze@hotmail.com>
2020-09-07 20:55:22 +0200

    add conditionals

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -8,18 +8,32 @@ const pizzaPrice = 80
 
 console.log(vegetarian, hawaiian, pepperoni);
 
+//2
+
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+//3
 
 const orderName = prompt ("Enter the name of the pizza you want to order today.");
 
+//5
+
+if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
+    const orderQuantity = prompt (`How many of ${orderName} do you want?`);
+} else {
+    alert('Select a pizza from the menu');
+}
+
 console.log(orderName);
 
-const orderQuantity = prompt (`How many of ${orderName} do you want?`);
-
 console.log(orderQuantity);
+
+//4
 
 const orderTotal = (pizzaPrice * orderQuantity);
 
 console.log(orderTotal)
 
 alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`)
+
+
