bd3592c28d27826a9068b07345ed640d476a84ff
Evelina Berg <evelina.berg@outlook.com>
2020-09-07 21:36:05 +0200

    Iteration 3 and 4

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,3 +5,37 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 //Put your Javscript code here:
+
+// Iteration 1
+console.log(vegetarian);
+console.log(hawaiian);
+console.log(pepperoni);
+console.log(pizzaPrice);
+
+// Iteration 2
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+
+// Iteration 3 
+
+const orderName = prompt('Enter the name of the pizza you want to order today.');
+
+const orderQuantity = prompt(`How many of ${orderName} Pizzas do you want?`);
+
+// Iteration 4
+
+const orderTotal = orderQuantity * pizzaPrice
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr.`);
+
+// Iteration 5
+
+
+
+
+if (orderName ==){
+    document.getElementById("demo").innerHTML =
+    "How many " + orderName + "do you want";
+  }
+
+else 
