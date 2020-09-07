bafe927cf6ba34e4dc0b2b9bc3b9ed46d54c2731
annahasselstrom <64977313+annahasselstrom@users.noreply.github.com>
2020-09-07 15:36:58 +0200

    Completed iteration 3. Added two const variables for choice and quantity

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -13,7 +13,14 @@ const pizzaPrice = 80
 console.log(pizzaPrice);
 
 //const welcomeMessage = () => {
-    alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+   //alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);//
 //};
 
 //welcomeMessage();
+
+//Variables for inserting choice and quantity of pizza using the prompt method.//
+
+const orderName = prompt("Enter the name of the pizza you want to order today"); 
+const orderQuantity = prompt(`How many of ${orderName} do you want?`); 
+
+
