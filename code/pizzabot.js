c1c60864e02b0803488844bfb636b7bdffbb8b3f
Corneliaryas <hello@corneliaryas.com>
2020-09-07 21:37:37 +0200

    Iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,6 +4,7 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
+
 console.log(vegetarian);
 console.log(hawaiian);
 console.log(pepperoni);
@@ -15,10 +16,56 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 const orderName = prompt("Enter the name of the pizza you want to order today.");
 
-
 let orderQuantity ;
 
-if (orderName === vegetarian) {
+/*let validateOrderName = (orderName) => {
+    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
+        return true;
+    }    else {
+        return false;
+    }
+
+};*/
+
+const validateOrderName = (pizzaName) => {
+    if ((pizzaName === hawaiian) || (pizzaName === pepperoni ) || (pizzaName === vegetarian)) {
+    orderQuantity = prompt(`How many of ${orderName} do you want?`);
+}   else {
+    alert("Select a pizza from the menu"); 
+    
+}
+
+}
+
+validateOrderName(orderName);
+
+
+const calculateTotalCost = (num1, num2) => {
+    return num1 * num2;
+}
+
+const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
+
+
+
+const calculateCookingTime = (quantity) => {
+
+    if (quantity <= 2) {
+        return 10;
+    } else if (quantity > 2 && quantity < 6) {
+        return 15;
+    } else {
+        return 20;
+    };
+}
+
+let cookingTime = calculateCookingTime(orderQuantity); 
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal}kr. The pizza(s) will take ${cookingTime} minutes.`);
+
+/*
+
+if (orderName === vegetarian ) {
     orderQuantity = prompt(`How many of ${orderName} do you want?`);
 
 } else if (orderName === hawaiian) {
@@ -44,3 +91,4 @@ if (orderQuantity <= 2) {
 const orderTotal = orderQuantity * pizzaPrice;
 alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal}kr. The pizza(s) will take ${cookingTime} minutes.`);
 
+*/
