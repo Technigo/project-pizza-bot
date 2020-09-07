396326c09ab606d48626f9557416af519013d5da
MindstormingAB <s.elander@mindstorming.se>
2020-09-07 16:31:11 +0200

    Third iteration - Ask the customer

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -13,3 +13,9 @@ console.log(pizzaPrice);
 //Put your Javscript code here:
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
+
+const orderName = prompt('Enter the name of the pizza you want to order today.')
+console.log(orderName)
+
+const orderQuantity = prompt(`How many of ${orderName} do you want?`)
+console.log(orderQuantity)
