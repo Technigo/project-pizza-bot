a9816be4c8459849f09f325e26f56498308dc8e4
LINDA HINTZE <lindahintze@hotmail.com>
2020-09-07 19:56:11 +0200

    add prompt to ask customer + finalize order with alert

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -9,3 +9,17 @@ const pizzaPrice = 80
 console.log(vegetarian, hawaiian, pepperoni);
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+const orderName = prompt ("Enter the name of the pizza you want to order today.");
+
+console.log(orderName);
+
+const orderQuantity = prompt (`How many of ${orderName} do you want?`);
+
+console.log(orderQuantity);
+
+const orderTotal = (pizzaPrice * orderQuantity);
+
+console.log(orderTotal)
+
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`)
