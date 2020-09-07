ec694635b549cbe2ba4eff62572ca815e2aaf2ba
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2020-09-07 14:30:53 +0200

    Iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,16 +6,26 @@ const pizzaPrice = 80
 
 //Put your Javscript code here:
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
+/*Funktioner*/
 
-alert(`Hello! Happy to serve you pizza. On our menu we have ${vegetarian}, ${pepperoni}, ${hawaiian}`);
+/*Check pizza name*/ 
+const validateOrderName = (orderName) =>{
+    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni)
+    {
+        return true;
+    }
+    else return false;
+}
 
-const orderName = prompt("Enter the name of the pizza you want to order");
-
-if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
-    const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+/*Calculate the cost of order*/ 
+const calculateTotalCost = (orderQuantity, pizzaPrice) =>{
     const orderTotal = orderQuantity * pizzaPrice;
+    return orderTotal;
+}
+
+const calculateCookingTime = (orderQuantity) =>{
+
     let cookingTime;
-    console.log(orderQuantity);
 
     if (orderQuantity >= 1 && orderQuantity <=2) {
         cookingTime = 10;
@@ -24,11 +34,27 @@ if (orderName === vegetarian || orderName === hawaiian || orderName === pepperon
     } else if (orderQuantity >= 6) {
         cookingTime = 20;
     } else {
-        alert("You must order more pizza!");
+        cookingTime = 0;
     }
+    return cookingTime;
+}
 
+
+
+alert(`Hello! Happy to serve you pizza. On our menu we have ${vegetarian}, ${pepperoni}, ${hawaiian}`);
+
+const orderName = prompt("Enter the name of the pizza you want to order");
+
+if (validateOrderName(orderName)===true) {
+
+    const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+    const orderTotal = calculateTotalCost(orderQuantity);
+
+    let cookingTime = calculateCookingTime(orderQuantity);
+   
     alert(`Great, I'll get started on your ${orderName} right away. It will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`);
-} else {
+}
+ else {
     alert("Select a pizza from the menu");
 }
 
