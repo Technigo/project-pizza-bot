5d13d359ca9dfe5d48cfcff581368114954ed072
Gabbi-89 <gabriellabolin@hotmail.com>
2020-09-08 10:02:17 +0200

    All iterations done

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,40 +4,66 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-//ITERATION 1
-console.log(vegetarian, hawaiian, pepperoni);
+console.log(vegetarian, hawaiian, pepperoni)
 
-//ITERATION 2
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+// Welcome message
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
 
-//ITERATION 3
-orderName = prompt(`Enter the name of the pizza you want to order today. Choose among ${vegetarian}, ${hawaiian} or ${pepperoni}.`);
 
-//ITERATION 5
-if (orderName === "vegetarian") {
-    prompt(`How many of ${orderName} do you want?`);
-   } else {
-    prompt("Please choose one of the pizzas from our menu!");
+// Order-function - choose pizza + amount, if error occurs it will restart function
+let orderName = ""
+let orderQuantity = 1
+
+orderName = prompt(`Enter the name of the pizza you want to order today. Choose among ${vegetarian}, ${hawaiian} or ${pepperoni}.`)
+
+let validateOrderName = () => {
+    
+    let orderQuantityFunction = () => {
+        orderQuantity = prompt(`How many of ${orderName} do you want?`)
+    }
+    
+    if (orderName.match(/vegetarian/g)) {
+        orderQuantityFunction()
+    } else if (orderName.match(/hawaiian/g)) {
+        orderQuantityFunction()
+    } else if (orderName.match(/pepperoni/g)) {
+        orderQuantityFunction()
+    } else {
+        console.log("wrong name")
+        orderName = prompt(`Please choose a pizza from our menu. Choose among ${vegetarian}, ${hawaiian} or ${pepperoni}.`)
+        validateOrderName()
+    }
 }
 
-orderQuantity = prompt(`How many of ${orderName} do you want?`);
-
-console.log(orderName);
-console.log(orderQuantity);
-
-//ITERATION 4
-orderTotal = orderQuantity * pizzaPrice
+validateOrderName()
+console.log(orderName)
+console.log(orderQuantity)
 
 
-//ITERATION 5
-let cookingTime = 10
+// Total cost-function
+let orderTotal = 1
 
-if (orderQuantity >= 3) {
-    cookingTime = 15
-} else if (orderQuantity >= 6) {
-    cookingTime = 20
+let calculateTotalCost = () => {
+    orderTotal = orderQuantity * pizzaPrice
+}
+calculateTotalCost()
+console.log(orderTotal)
+
+// Total cooking-time-function
+let cookingTime = 1
+
+let calculateCookingTime = () => {
+    if (orderQuantity < 3) {
+        cookingTime = 10
+    } else if (orderQuantity < 6) {
+        cookingTime = 15
+    } else {
+        cookingTime = 20
+    }
 }
 
-alert(`Great, I'll get started on your ${orderName}(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`);
+calculateCookingTime()
+console.log(cookingTime)
 
-
+// Confirmation message
+alert(`Great, I'll get started on your ${orderName}(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`)
