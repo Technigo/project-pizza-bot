834ab46a36e34ca4bb9ea7fcc87d25686b67e723
Amanda Flodström <amandaflodstroem@gmail.com>
2020-09-08 09:58:05 +0200

    Iteration 4

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -14,5 +14,6 @@ const orderName = prompt("Enter the name of the pizza you want to order today.")
 
 const orderQuantity = prompt(`How many of ${orderName} do you want?`)
 
-
+const orderTotal = orderQuantity * pizzaPrice
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr.`)
 
