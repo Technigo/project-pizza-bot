422091736eb3ffcdebb16d21221f98589e58c117
MindstormingAB <s.elander@mindstorming.se>
2020-09-08 16:59:49 +0200

    Adjustements before iteration 5

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,22 +4,27 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-// Console logs
-console.log(vegetarian);
-console.log(hawaiian);
-console.log(pepperoni);
-console.log(pizzaPrice);
+// Iteration 1 - Setting everything up
 
-//Put your Javscript code here:
+// console.log(vegetarian);
+// console.log(hawaiian);
+// console.log(pepperoni);
+// console.log(pizzaPrice);
+
+// Iteration 2 - Greetings
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
 
-const orderName = prompt('Enter the name of the pizza you want to order today.')
-console.log(orderName)
+// Iteration 3 - Ask the customer
 
-const orderQuantity = prompt(`How many of ${orderName} pizza(s) do you want?`)
-console.log(orderQuantity)
+const orderName = prompt('Enter the name of the pizza you want to order today.');
+// console.log(orderName);
 
-const orderTotal = orderQuantity * pizzaPrice
+const orderQuantity = prompt(`How many of ${orderName}(s) do you want?`)
+// console.log(orderQuantity)
 
-alert(`Great, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr.`)
+// Iteration 4 - Finalize the order
+
+const orderTotal = orderQuantity * pizzaPrice;
+
+alert(`Great, I'll get started on your ${orderName}(s) right away, it will cost ${orderTotal} kr.`);
