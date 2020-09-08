c7913e6bacf4eb5ba9ed4ff1201703491d873573
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2020-09-08 15:59:15 +0200

    Iteration 5 changed validateName if statement to include ()

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -7,8 +7,8 @@ const pizzaPrice = 80
 //Put your Javscript code here:
 console.log(vegetarian,hawaiian,pepperoni);
 console.log(pizzaPrice);
-
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+document.getElementById("welcome").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+/*alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);*/
 const orderName = prompt('Enter the name of the pizza you want to order today.');
 console.log(orderName);
 const orderQuantity = prompt(`How many of ${orderName} do you want?`);
@@ -36,7 +36,7 @@ const validateOrderName = (orderName) => {
 validateOrderName(orderName); 
 console.log(validateOrderName(orderName));
 
-if (validateOrderName === true) {
+if (validateOrderName(orderName) === true) {
     alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and the pizzas will take ${cookingTime} minutes`); 
 } else {
     alert(`Sorry, we don't have that pizza. Choose a pizza from the menu`);
