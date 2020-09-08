ccbedfc2b1de9eea9a74343b994a1a80f8b45208
Sara Sundblom <sundblomsara@gmail.com>
2020-09-08 11:58:08 +0200

    trying to add a return

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ alert(
 );
 
 let orderName = prompt("Enter the name of the pizza you want to order today.. ");
+
+let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+console.log(orderQuantity);
+
+let orderTotal = orderQuantity * pizzaPrice;
+
+console.log(orderTotal);
+
+
+
+
+  if ((orderName === "vegetarian", "hawaiian", "pepperoni")) {
+    orderQuantity;
+  } else {
+    alert("Order something else");
+    return [orderName];
+  }
+  
+  let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+  console.log(orderQuantity);
+  
+  let orderTotal = orderQuantity * pizzaPrice;
+  console.log(orderTotal);
+  
+  alert(
+    ` Great! I'll get started on your ${orderName} right away!, it will cost ${orderTotal} kr`
+  );
+  
+
