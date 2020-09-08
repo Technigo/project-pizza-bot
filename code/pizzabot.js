fb4d1068154d7f1ea73067f999be33a5d2121790
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-09-08 10:11:29 +0200

    Clean up code, start to create separate functions

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -7,11 +7,11 @@ const pizzaPrice = 80
 //Put your Javscript code here:
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
 
-let orderName = prompt('Enter the name of the pizza you want to order today.');
+let orderName = prompt('Enter the name of the pizza you want to order today.'); //Turn into an HTML form
 
 //Function to check if the entered name is a valid pizza on the menu - toLowerCase in case the user has entered with caps
-if (orderName.toLowerCase() === "hawaiian" || orderName.toLowerCase() ==="pepperoni" || orderName.toLowerCase() === "vegetarian") {
-    let orderQuantity = prompt(`How many ${orderName} do you want?`);
+if (confirmName(orderName)) {
+    let orderQuantity = prompt(`How many ${orderName} do you want?`);  //Turn into an HTML form
 
     let orderTotal = pizzaPrice * orderQuantity;
 
@@ -31,4 +31,32 @@ if (orderName.toLowerCase() === "hawaiian" || orderName.toLowerCase() ==="pepper
 
 } else {
     alert('Please select a Pizza from the menu.');
+};
+
+
+
+
+//Function to check if user's input pizza choice is correct
+let confirmName = (pizzaName) => {
+    if (pizzaName.toLowerCase() === "hawaiian" || pizzaName.toLowerCase() ==="pepperoni" || pizzaName.toLowerCase() === "vegetarian") {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//Function to caltulate Total preparation time
+let prepTime = (amount) => {
+    if(amount <= 2) {
+        return 10
+    } else if (amount >= 3 && amount <= 5) {
+        return 15
+    } else {
+        return 20
+    }
+};
+
+//Function to calculate Total Cost
+let totalCost = (quantity) => {
+    return quantity * pizzaPrice
 };
