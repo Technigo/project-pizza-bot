b5a6224bc71f48f09de572d1a05e3fe713c4f9d4
Gabbi-89 <gabriellabolin@hotmail.com>
2020-09-08 14:04:22 +0200

    End of Tuesday session

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,20 +1,22 @@
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
+const kebab = "Kebab Pizza"
 
 const pizzaPrice = 80
 
-console.log(vegetarian, hawaiian, pepperoni)
+console.log(vegetarian, hawaiian, pepperoni, kebab)
 
 // Welcome message
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
-
+// alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, ${pepperoni} and ${kebab}.`)
+document.getElementById("welcome").innerHTML = `On our menu we have ${vegetarian}, ${hawaiian}, ${pepperoni} and ${kebab}.`
 
 // Order-function - choose pizza + amount, if error occurs it will restart function
 let orderName = ""
 let orderQuantity = 1
 
-orderName = prompt(`Enter the name of the pizza you want to order today. Choose among ${vegetarian}, ${hawaiian} or ${pepperoni}.`)
+// orderName = prompt(`Enter the name of the pizza you want to order today. Choose among ${vegetarian}, ${hawaiian}, ${pepperoni} or ${kebab}.`)
+document.getElementById("quantity").innerHTML = `Ok! How many of ${orderName} do you want?`
 
 let validateOrderName = () => {
     
@@ -28,14 +30,17 @@ let validateOrderName = () => {
         orderQuantityFunction()
     } else if (orderName.match(/pepperoni/g)) {
         orderQuantityFunction()
-    } else {
+    } else if (orderName.match(/kebab/g)) {
+        orderQuantityFunction()
+    }
+    else {
         console.log("wrong name")
-        orderName = prompt(`Please choose a pizza from our menu. Choose among ${vegetarian}, ${hawaiian} or ${pepperoni}.`)
+        orderName = prompt(`Please choose a pizza from our menu. Choose among ${vegetarian}, ${hawaiian}, ${pepperoni} or ${kebab}.`)
         validateOrderName()
     }
 }
 
-validateOrderName()
+// validateOrderName()
 console.log(orderName)
 console.log(orderQuantity)
 
@@ -46,8 +51,10 @@ let orderTotal = 1
 let calculateTotalCost = () => {
     orderTotal = orderQuantity * pizzaPrice
 }
+
 calculateTotalCost()
 console.log(orderTotal)
+
 
 // Total cooking-time-function
 let cookingTime = 1
@@ -66,4 +73,4 @@ calculateCookingTime()
 console.log(cookingTime)
 
 // Confirmation message
-alert(`Great, I'll get started on your ${orderName}(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`)
+document.getElementById("confirmation").innerHTML = `Great, I'll get started on your ${orderName}(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`
