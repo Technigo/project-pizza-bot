17d0aece97b052fad17cbd1df6412fd2348d6df6
Sara Sundblom <sundblomsara@gmail.com>
2020-09-08 10:10:20 +0200

    first two steps

---
+++ script.js
@@ -0,0 +1,9 @@
+const pizzaPrice = 80;
+
+console.log(vegetarian, hawaiian, pepperoni, pizzaPrice);
+
+alert(
+  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+);
+
+let orderName = prompt("Enter the name of the pizza you want to order today.. ");
