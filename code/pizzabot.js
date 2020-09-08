b55812a94e78f5ce860d5c6cd620a312ad0c2741
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-09-08 12:30:25 +0200

    Add CSS file

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -23,6 +23,8 @@ if (confirmName(orderName)) {
 
     let confirmText = document.getElementById('confirm');
     let pizzaPhoto = document.getElementById('pizza-photo');
+    confirmText.style.display = "block";
+    pizzaPhoto.style.display = "block";
     confirmText.innerHTML = `Great ${customerName}, I'll get started on your ${orderName} Pizza(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take ${orderTime} minutes.`;
     pizzaPhoto.src = pizzaImage(orderName); // Calls pizzaImage function to show the ordered Pizza's image
 
