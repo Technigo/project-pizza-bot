65d6bed649aec9761bc025f30cf383878c2e0aaa
nathalie <nathalie-nordstrom@hotmail.com>
2020-09-08 08:47:39 +0200

    one

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -2,6 +2,15 @@ const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
-const pizzaPrice = 80
+const pizzaPrice = 8
 
 //Put your Javscript code here:
+console.log(pepperoni)
+
+alert("Hey! Happy to serve your pizza. On our menu we have, Vegetarian Pizza, Hawaiian Pizza, Pepperoni Pizza")
+
+prompt("Enter the name of the pizza you want to order today")
+
+const orderName = 
+
+prompt("How many of ${orderName} do you want?") 
