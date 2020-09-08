04856faa6b4744418d8a0a4651ac2fb9c5f6c631
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-08 14:24:46 +0200

    Added functions for each iteration

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -2,45 +2,73 @@ const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
-//Make variable easier - if we want 
+//Make variable easier - if we can 
 
-const pizzaPrice = 80
+// const pizzaPrice = 80
 
-//Put your Javscript code here:
 
-// ITERATION 2
+// GREETING
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${hawaiian}, ${vegetarian} and ${pepperoni}`);
 
-// ITERATION 3
 
+// NAME OF PIZZA + IF NOT TRUE THEN ALERT TO USER ASKING THEM TO ORDER SOMETHING OFF THE MENU
 let orderName = prompt('Enter the name of the pizza you want to order today.');
-if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
-    orderQuantity = prompt(`How many of ${orderName} do you want?`)
-} else  {
-    alert("Sorry you have to order something of the menu")
-}
+
+const validateOrderName = (orderName) => {
+    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni){
+        return true; 
+    }
+    else { 
+        alert("Sorry you have to order something off the menu");
+    return false;
+    }
+};
+
+validateOrderName(orderName);
 
 
-    
-let cookingTime = prompt(`How many of ${orderName} do you want?`);
-if (orderQuantity <= 2) {
-    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take 10 minutes`);
-}
+
+// HOW MANY DO YOU WANT TO ORDER? + CALCULATION
+let orderQuantity = prompt( `How many of ${orderName} do you want?`);
+const pizzaPrice = 80;
+
+const calculateTotalCost = (orderQuantity, pizzaPrice) => {
+    const orderTotal = (orderQuantity * pizzaPrice);
+};
+
+calculateTotalCost (orderQuantity, pizzaPrice);
 
 
-// ITERATION 4
 
-const orderTotal = (orderQuantity * pizzaPrice);
+// FINALISE ORDER; SHOW PIZZA TYPE, ORDER TOTAL AND COOKING TIME
+const calculateCookingTime = () => {
+    if (orderQuantity <= 2) {
+        alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take 10 min.`);
+    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+        alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 15 min.`);
+    } else {
+        alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 20 min.`);
+    }
+};
 
-alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`);
+calculateCookingTime(orderQuantity);
 
-// ITERATION 5 
 
-// const orderName = prompt('Enter the name of the pizza you want to order today.');
-// if (orderName === orderName) {
-//     const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+// if (pizzaIsOnMenu === true) {
+//     let orderQuantity = prompt( `How many of ${orderName} do you want?`);
+//     const orderTotal = (orderQuantity * pizzaPrice);
+
+//     if (orderQuantity <= 2) {
+//         alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take 10 min.`);
+//     } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+//         alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 15 min.`);
+//     } else {
+//         alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 20 min.`);
 //     }
-// else alert("Sorry order something from our menu");
+// }
+ 
+
+
 
 
