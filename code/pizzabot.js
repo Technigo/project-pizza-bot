51f66bf134caa38336efe54bebc6c34c06cb72eb
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-09 13:56:54 +0200

    3rd Iteration completed

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,7 +5,10 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-
-
 // Iteration 2 - Greet the customer
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
+
+// Iteration 3 - Ask the customer
+const orderName = prompt(`Enter the name of the pizza you want to order today.`)
+
+const orderQuantity = prompt(`How many of ${orderName} do you want?`)
