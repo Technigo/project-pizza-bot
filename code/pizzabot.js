f1ebd5e79c92cfac865a27ca92a737c26c7aeea0
Amanda Flodström <amandaflodstroem@gmail.com>
2020-09-09 15:50:24 +0200

    Iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -12,24 +12,46 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawa
 
 let orderName = prompt("Enter the name of the pizza you want to order today.")
 
-if (orderName !== vegetarian && orderName !== hawaiian && orderName !== pepperoni) {
-  alert("Select a pizza from the menu")
-  orderName = prompt("Enter the name of the pizza you want to order today.")
+const validateOrderName = (orderName) => {
+  if (orderName !== vegetarian && orderName !== hawaiian && orderName !== pepperoni) {
+    alert("Select a pizza from the menu")
+    return false
+  }
+  else {
+    return true
+  }
 }
 
-const orderQuantity = prompt(`How many of ${orderName} do you want?`)
 
-const orderTotal = orderQuantity * pizzaPrice
 
-let cookingTime
+if (validateOrderName(orderName)) {
 
-if (orderQuantity <= 2) {
-  cookingTime = "10 minutes"
-} else if (orderQuantity <= 5) {
-  cookingTime = "15 minutes"
-} else {
-  cookingTime = "20 minutes"
+  const orderQuantity = prompt(`How many of ${orderName} do you want?`)
+  let orderTotal
+
+  calculateTotalCost = () => {
+    orderTotal = orderQuantity * pizzaPrice
+  }
+
+  calculateTotalCost()
+
+  let cookingTime
+
+  calculateCookingTime = () => {
+    if (orderQuantity <= 2) {
+      cookingTime = "10 minutes"
+    }
+
+    else if (orderQuantity >= 3 && orderQuantity <= 5) {
+      cookingTime = "15 minutes"
+    }
+
+    else {
+      cookingTime = "20 minutes"
+    }
+  }
+  calculateCookingTime()
+
+  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take ${cookingTime}.`)
+
 }
-
-alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take ${cookingTime}.`)
-
