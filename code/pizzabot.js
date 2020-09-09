c10cf40564b6fc682e22d300aceca21c05127757
Evelina Berg <evelina.berg@outlook.com>
2020-09-09 09:24:38 +0200

    Innan sista funktionen läggs till

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,6 +6,38 @@ const pepperoni = "Pepperoni"
 
 const pizzaPrice = 80
 
+const validateOrderName = (orderName) => {
+    if (orderName.toLowerCase() === vegetarian.toLowerCase()) {
+        return true;
+
+    } else if (orderName.toLowerCase() === hawaiian.toLowerCase()) {
+        return true;
+    
+    } else if (orderName.toLowerCase() === pepperoni.toLowerCase()) {
+        return true;
+    
+    } else {
+        alert('Select a pizza from the menu!');
+        return false;
+    }
+};
+
+const calculateTotalCost = (pizzaName) => {
+    let orderTotal = orderQuantity * pizzaPrice;
+    return orderTotal;
+}
+
+const calculateCookingTime = (orderQuantity) => {
+    let orderTime = 0; 
+    if (orderQuantity <= 2) {
+        orderTime = 10;
+    } else if (orderQuantity < 6 || orderQuantity > 2) {
+        orderTime = 15;
+    } else {
+        orderTime = 20;
+    }
+}
+
 //Put your Javscript code here:
 
 // Iteration 1
@@ -20,63 +52,15 @@ console.log(pizzaPrice);
 // Iteration 2
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+
 const orderName = prompt('Enter the name of the pizza you want to order today.');
+const isValidOrder = validateOrderName(orderName);
+const orderQuantity = prompt(`How many of ${orderName} Pizzas do you want?`);
 
-const validateOrderName = (orderName) => {
-    if (orderName.toLowerCase() === vegetarian.toLowerCase()) {
-        orderDetails(orderName);
-        return true;
-
-    } else if (orderName.toLowerCase() === hawaiian.toLowerCase()) {
-        orderDetails(orderName);
-        return true;
-    
-    } else if (orderName.toLowerCase() === pepperoni.toLowerCase()) {
-        orderDetails(orderName);
-        return true;
-    
-    } else {
-        alert('Select a pizza from the menu!');
-        return false;
-    }
-};
-
-const orderDetails = (pizzaName) => {
-    const orderQuantity = prompt(`How many of ${pizzaName} Pizzas do you want?`);
-    const orderTotal = orderQuantity * pizzaPrice;
-    alert(`Great, I'll get started on your ${pizzaName} right away, it will cost ${orderTotal} kr.`);
+if (isValidOrder === true) {
+    const isTotalCost = calculateTotalCost(orderQuantity, pizzaPrice);
+    console.log(isTotalCost);
+    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take ${orderTime}`);
 }
+  
 
-if (validateOrderName === true) {
-    calculateTotalCost(orderQuantity, pizzaPrice);
-    alert(`Great, I'll get started on your ${pizzaName} right away, it will cost ${orderTotal} kr. The pizzas will take ${orderTime}`);
-}
-
-    const orderQuantity = prompt(`How many of ${pizzaName} Pizzas do you want?`);
-
-const calculateTotalCost = (orderQuantity, pizzaPrice) => {
-    const orderTotal = orderQuantity * pizzaPrize;
-    return orderTotal;
-}
-
-
-
-// Iteration 3 
-const orderDetails = (pizzaName) => {
-    const orderQuantity = prompt(`How many of ${pizzaName} Pizzas do you want?`);
-    
-    alert(`Great, I'll get started on your ${pizzaName} right away, it will cost ${orderTotal} kr. The pizzas will take ${orderTime}`);
-}
-
-// Iteration 3 
-
-
-
-
-
-
-
-
-    if (orderQuantity <= 2) {
-        orderTime === 10;
-    } else if (orderQuantity < 6 || orderQuantity > 2); 
