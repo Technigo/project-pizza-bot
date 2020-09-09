5c9290133d5fca0c8be814980eb78ce34c5134eb
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2020-09-09 14:20:29 +0200

    added function to hidden and open part of the form

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -20,7 +20,7 @@ const decidePizza = "Enter the name of the pizza you want to order today!";
 document.getElementById("name-of-pizza").innerHTML = decidePizza;
 
 //How many Pizza's
-const orderQuantityMsg = `How many do you want?`;
+let orderQuantityMsg = `How many do you want?`;
 document.getElementById("quantity-of-pizza").innerHTML = orderQuantityMsg;
 
 // Function for pizza choice/order
@@ -34,40 +34,30 @@ let addOrder = (event) => {
   document.getElementById("form-number").value = "";
 
   if (validateOrderName(orderName)) {
-    // orderQuantityMsg = `How many of ${orderName} do you want?`;
-    // document.getElementById("quantity-of-pizza").innerHTML = orderQuantityMsg;
-    const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
-    console.log(orderTotal);
-
-    const cookingTime = calculateCookingTime(orderQuantity);
-    console.log(cookingTime);
-
-    const finalMessage = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza(s) will take ${cookingTime} minutes!`;
-    console.log(finalMessage);
-
-    // Final message
-    document.getElementById("final-message").innerHTML = finalMessage;
+    orderQuantityMsg = `How many of ${orderName} do you want?`;
+    document.getElementById("quantity-of-pizza").innerHTML = orderQuantityMsg;
+    toggleQuantityInput();
   } else {
     const errorMessage = "Please, select a pizza from the our menu";
     document.getElementById("errorMessage").innerHTML = errorMessage;
     return;
   }
 
-  // const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
-  // console.log(orderTotal);
+  const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
+  console.log(orderTotal);
 
-  // const cookingTime = calculateCookingTime(orderQuantity);
-  // console.log(cookingTime);
+  const cookingTime = calculateCookingTime(orderQuantity);
+  console.log(cookingTime);
 
-  // const finalMessage = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza(s) will take ${cookingTime} minutes!`;
-  // console.log(finalMessage);
+  const finalMessage = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza(s) will take ${cookingTime} minutes!`;
+  console.log(finalMessage);
 
-  // // Final message
-  // document.getElementById("WelcomeP").innerHTML = finalMessage;
+  // Final message
+  document.getElementById("final-message").innerHTML = finalMessage;
 };
 
 // Button for pizza choice
-document.getElementById("button").addEventListener("click", addOrder);
+document.getElementById("button-1").addEventListener("click", addOrder);
 
 //Add function to validate pizza order
 const validateOrderName = (orderName) => {
@@ -97,6 +87,13 @@ const calculateCookingTime = (orderQuantity) => {
   } else {
     return 20;
   }
+};
+
+// Function to reveal quantity input
+const toggleQuantityInput = () => {
+  document
+    .getElementById("test")
+    .classList.toggle("quantity-of-pizza-wrapper-active");
 };
 
 // if (validateOrderName(pizzaName)) {
