4e2732bf06bdf39a342c14887456b3b4b560a351
Emelie Svensson <emeliesv@live.se>
2020-09-09 10:29:23 +0200

    fixed empty string return by adding new variable

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -2,58 +2,60 @@
 const hawaiian = document.getElementById('hawaiianPizza')
 const pepperoni = document.getElementById('pepperoniPizza') */
 
+const vegetarian = "vegetarian pizza";
+const hawaiian = "hawaiian pizza";
+const pepperoni = "pepperoni pizza";
 
-const vegetarian = "vegetarian pizza"
-const hawaiian = "hawaiian pizza"
-const pepperoni = "pepperoni pizza"
-
-const pizzaPrice = 80
+const pizzaPrice = 80;
 
 //Put your Javscript code here:
-console.log(vegetarian, hawaiian, pepperoni)
+console.log(vegetarian, hawaiian, pepperoni);
 
-alert(`Hey, happy to serve your pizza! In our menu we have: ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+alert(
+  `Hey, happy to serve your pizza! In our menu we have: ${vegetarian}, ${hawaiian} and ${pepperoni}`
+);
 
-  let isCorrect = false
-  let orderName = ""
-
-const validateOrderName = (orderName) => {
-  
-  while (isCorrect==false){
-  
-    orderName = prompt("Enter the name of the pizza you want to order today", 
-    "Enter Pizza")
+const validateOrderName = () => {
+  let isCorrect = false;
+  let orderName = "";
+  while (isCorrect === false) {
+    orderName = prompt(
+      "Enter the name of the pizza you want to order today",
+      "Enter Pizza"
+    );
     orderName = orderName.toLowerCase();
-   
-    isCorrect = 
-      orderName === vegetarian || 
-      orderName === hawaiian || 
-      orderName === pepperoni 
-   
-      isCorrect 
+
+    isCorrect =
+      orderName === vegetarian ||
+      orderName === hawaiian ||
+      orderName === pepperoni;
+
+    isCorrect
       ? alert(`You have ordered ${orderName}`)
-      : alert(`Select a pizza from the menu please`)
+      : alert(`Select a pizza from the menu please`);
   }
-}
-//Returns an empty string on line 56
-validateOrderName()
+  return orderName
+};
 
-let orderQuantity = prompt(`How many of ${orderName} do you want?`, `Number of pizza`)
+const validOrderName = validateOrderName();
 
+let orderQuantity = prompt(
+  `How many of ${validOrderName} do you want?`,
+  `Number of pizza`
+);
 
 const calculateCookingTime = (orderQuantity) => {
-if (orderQuantity > 0 && orderQuantity <=2) {
-  return 10 
-  } else if (orderQuantity >= 3 && orderQuantity <=5) {
-  return 15 
+  if (orderQuantity > 0 && orderQuantity <= 2) {
+    return 10;
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    return 15;
   } else {
-  return 20 
+    return 20;
   }
-}
-console.log (orderQuantity) 
+};
+console.log(orderQuantity);
 
-let orderTotal = orderQuantity*pizzaPrice
-alert(`Great! I'll get started on your ${orderName}, It will cost you ${orderTotal} kr. The pizza(s) will take ${calculateCookingTime()} minutes.`) 
-
-
-
+let orderTotal = orderQuantity * pizzaPrice;
+alert(
+  `Great! I'll get started on your ${validOrderName}, It will cost you ${orderTotal} kr. The pizza(s) will take ${calculateCookingTime()} minutes.`
+);
