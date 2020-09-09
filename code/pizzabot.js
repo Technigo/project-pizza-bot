856f2ed9ac50c1c87e43330701b643db0d226bf4
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2020-09-09 20:05:22 +0200

    started on fifth iteration, work in progress

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -31,3 +31,30 @@ alert("Great, I'll get started on your " + orderName + " pizza(s) right away, it
 
 // fifth iteration
 
+/* When the user enters the `orderName` prompt,
+add an if-statement to check if the input text matches
+any of the pizza name variables (`vegetarian`, `hawaiian`, 
+pepperoni`). If yes - show the next prompt,
+if no - show an alert that says `Select a pizza from the menu` 
+or something similar.
+
+### **Show cooking times**
+
+Before you print the final `Great, I'll get started on your X right away, it will cost Y kr` message to the user, calculate the cooking time based on these rules and add it to the message:
+
+- 1-2 pizzas: `The pizzas will take 10 minutes.`
+- 3-5 pizzas: `The pizzas will take 15 minutes.`
+- 6+ pizzas: `The pizzas will take 20 minutes.`
+
+*/
+
+
+const checkOrderName = () => {
+
+}
+
+if (orderName.toLowerCase() === vegetarian || orderName.toLowerCase() === hawaiian || orderName.toLowerCase() === pepperoni) {
+    return true;
+} else {
+   
+}
