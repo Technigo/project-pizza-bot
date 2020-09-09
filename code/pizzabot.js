8644b7692940efece8333bf057d7ed00bd42df52
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-09 14:39:32 +0200

    Updated HTML & JavaScript as well as add css file

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -2,74 +2,81 @@ const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
-//Make variable easier - if we can 
+const pizzaPrice = 80;
 
-// const pizzaPrice = 80
+// Greeting giving the user the menu
+document.getElementById("greeting").innerHTML = `Hey! Welcome to the pizza bot! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`;
 
+// Enter name of pizza you want to order
+let orderName = document.getElementById("choosePizza").innerHTML = "Choose the name of the pizza you want to order today";
 
-// GREETING
-
-alert(`Hey! Happy to serve your pizza. On our menu we have ${hawaiian}, ${vegetarian} and ${pepperoni}`);
-
-
-// NAME OF PIZZA + IF NOT TRUE THEN ALERT TO USER ASKING THEM TO ORDER SOMETHING OFF THE MENU
-
-
-let orderName = prompt('Enter the name of the pizza you want to order today.');
-
-const validateOrderName = (orderName) => {
-    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni){
+//Function compares the paramater chosenPizza is true or false and will return which ever is true
+const validateOrderName = (chosenPizza) => {
+    if (chosenPizza.toLowerCase() === vegetarian.toLowerCase() || chosenPizza.toLowerCase() === hawaiian.toLowerCase() || chosenPizza.toLowerCase() === pepperoni.toLowerCase()){
         return true; 
     }
     else { 
-        alert("Sorry you have to order something off the menu");
-    return false;
+        return false;
     }
 };
 
-validateOrderName(orderName);
+//Calling the function and in the paramater we define what is in the orderName variable to then tell chosenPizza what pizza has been chosen so then it can check this against what is defined in the if and else statements. The outcome, true or false will e stored on the pizzaIsOnMenu variable.
+let pizzaIsOnMenu = validateOrderName(orderName);
 
-
-
-// HOW MANY DO YOU WANT TO ORDER? + CALCULATION
-let orderQuantity = prompt( `How many of ${orderName} do you want?`);
-const pizzaPrice = 80;
-
+//Function calculating the cost of the pizza based on the pizzaAmount and returning the amount.
 const calculateTotalCost = (orderQuantity, pizzaPrice) => {
     const orderTotal = (orderQuantity * pizzaPrice);
+    return orderTotal;     
 };
 
-calculateTotalCost (orderQuantity, pizzaPrice);
-
-
-
-// FINALISE ORDER; SHOW PIZZA TYPE, ORDER TOTAL AND COOKING TIME
-const calculateCookingTime = (orderQuantity, orderTotal) => {
-    if (orderQuantity <= 2) {
-        alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take 10 min.`);
-    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-        alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 15 min.`);
+const calculateCookingTime = (pizzaAmount) => {
+    if (pizzaAmount <= 2) {
+        return 10;
+    } else if (pizzaAmount >= 3 && pizzaAmount <= 5) {
+        return 15;
     } else {
-        alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 20 min.`);
+        return 20;
     }
 };
 
-calculateCookingTime(orderQuantity);
+//if statement which tells us if the user has put in a valid pizza name and if true then will create the promt asking them how many they want, which is then stored in the variable pizzaAmount. Otherwsie they will get the alert.
+if (pizzaIsOnMenu === true) {
+    let pizzaAmount = document.getElementById("howmany").innerHTML = `How many of ${orderName} do you want?`;
+    //Calling the function and in the parameter taking the data inside the pizzaAmount and pizzaPrice and using that to put into into the calculateCost function to then be able to do the calculation.
+    let totalPrice = calculateTotalCost(pizzaAmount, pizzaPrice);
+    let cookingTimeMin = calculateCookingTime(pizzaAmount);
+    document.getElementById("finalize").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${totalPrice} kr. The Pizzas will take ${cookingTimeMin}.`;
+} else {
+    document.getElementById("error").innerHTML = "Sorry you have to order something off the menu";
+}
 
 
-// if (pizzaIsOnMenu === true) {
-//     let orderQuantity = prompt( `How many of ${orderName} do you want?`);
-//     const orderTotal = (orderQuantity * pizzaPrice);
 
-//     if (orderQuantity <= 2) {
-//         alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take 10 min.`);
-//     } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-//         alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 15 min.`);
-//     } else {
-//         alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizzas will take 20 min.`);
-//     }
-// }
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
