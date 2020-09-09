fbcd3450a706f3a40ac8daa222902e307a8ad03d
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2020-09-09 18:37:01 +0200

    First commit

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,16 @@
+console.log('Is this working?');
+
 const vegetarian = "Vegetarian Pizza"
+console.log(vegetarian);
+
 const hawaiian = "Hawaiian Pizza"
+console.log(hawaiian);
+
 const pepperoni = "Pepperoni Pizza"
+console.log(pepperoni);
 
 const pizzaPrice = 80
+console.log(pizzaPrice);
 
 //Put your Javscript code here:
+
