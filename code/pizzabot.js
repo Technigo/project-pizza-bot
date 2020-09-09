b8f7777ec59d7cf06d59cabf9a339fce43861d9b
Sara Stjernberg <sara_stjernberg@hotmail.com>
2020-09-09 17:41:33 +0200

    connected form to js

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,54 +1,55 @@
-const vegetarian = "Vegetarian Pizza";
-const hawaiian = "Hawaiian Pizza";
-const pepperoni = "Pepperoni Pizza";
+const recieveData = (event) => {
+    event.preventDefault();
 
-const pizzaPrice = 80;
+    const typeOfPizza = validateOrderName();
+    const orderQuantity = document.getElementById("quantity").value;
+    const totalCost = calculateTotalCost(orderQuantity);
+    const cookingTime = calculateCookingTime(orderQuantity);
+    document.getElementById("message").innerHTML = (`Great, I'll get started on your ${typeOfPizza} right away, it will cost ${totalCost} kr. The pizzas will take ${cookingTime} minutes.`);
+    showImage(typeOfPizza);
+}
 
-//Iteration 2
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+let orderName;
+const validateOrderName = () => {
+    const vegetarian = document.getElementById("vegetarianPizza");
+    const hawaiian = document.getElementById("hawaiianPizza");
+    const pepperoni = document.getElementById("pepperoniPizza");
 
-//Iteration 3
-const orderName = prompt("Enter the name of the pizza you want to order today.");
-const trueFalse = validateOrderName(orderName);
-
-//Iteration 6 Add functions
-const validateOrderName = (orderName) => {
-    if (
-        orderName === vegetarian ||
-        orderName === hawaiian ||
-        orderName === pepperoni
-    ) {
-        console.log("true");
-        return true;
-
-
+    if (vegetarian.checked) {
+        return vegetarian.value;
+    } else if (hawaiian.checked) {
+        return hawaiian.value;
     } else {
-
-        console.log("false");
-        return false;
+        return pepperoni.value;
     }
 }
 
-
-if (trueFalse) {
-    const orderQuantity = prompt(`How many of ${orderName} do you want?`);
-    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`);
-} else {
-    alert("Select a pizza from the menu.");
+///Calculate total cost of the order.
+const calculateTotalCost = (orderQuantity) => {
+    const pizzaPrice = 80;
+    return orderQuantity * pizzaPrice;
 }
 
 
-calculateTotalCost = (orderQuantity, pizzaPrice) => {
-    const orderTotal = orderQuantity * pizzaPrice;
-}
-
-calculateCookingTime = (orderQuantity) => {
-    let cookingTime;
+//Calculate cooking time
+const calculateCookingTime = (orderQuantity) => {
+    let timeCooking;
     if (orderQuantity < 3) {
-        cookingTime = 10;
+        timeCooking = 10;
     } else if (orderQuantity > 5) {
-        cookingTime = 20;
+        timeCooking = 20;
     } else {
-        cookingTime = 15;
+        timeCooking = 15;
+    }
+    return timeCooking;
+}
+
+const showImage = (typeOfPizza) => {
+    if (typeOfPizza === "Vegetarian Pizza") {
+        document.getElementById("vegetarianImg").style.display = "block";
+    } else if (typeOfPizza === "Hawaiian Pizza") {
+        document.getElementById("hawaiianImg").style.display = "block";
+    } else {
+        document.getElementById("pepperoniImg").style.display = "block";
     }
 }
