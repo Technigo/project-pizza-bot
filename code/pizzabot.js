57dcbf6c2322f4fe51f20785dd8a3498152114ad
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2020-09-09 19:26:03 +0200

    fourth iteration

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -21,18 +21,13 @@ alert(`Hey! Happy to serve your pizza. On our menu we have ${x}, ${y} and ${z}.`
 
 // third iteration
 
-/*## **Iteration 3 - Ask the customer**
-
-Use `prompt()` to ask the user which pizza they want. The message in the prompt should read 
-`Enter the name of the pizza you want to order today.`. Their response should be 
-stored in a new variable called `orderName`.
-Next, ask the user (with another `prompt()`) how many they'd like: 
-`How many of X do you want?`. X should be replaced with the `orderName` 
-variable so that if I ordered "Hawaiian Pizza" it would read "How many Hawaiian Pizzas do you want?".
-As before, the response from this prompt should be stored in a variable, this time called `orderQuantity` */
-
 const orderName = prompt ('Enter the name of the pizza you want to order today.');
 const orderQuantity = prompt ("How many " + orderName + " pizzas do you want?");  
 
 // fourth iteration
+
+const orderTotal = orderQuantity * pizzaPrice;
+alert("Great, I'll get started on your " + orderName + " pizza(s) right away, it will cost " + orderTotal +" kr.");
+
+// fifth iteration
 
