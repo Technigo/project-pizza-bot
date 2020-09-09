d9707783b534e73a14d3884c4a91cec46e8a8cf9
LINDA HINTZE <lindahintze@hotmail.com>
2020-09-09 14:01:59 +0200

    add functions for all tasks

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,52 +1,65 @@
+
+// *** declairing variables
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
 
-//Put your Javscript code here:
+// *** Function 1 - Order Name 
+    const validateOrderName = (order) => {
+        if (order === vegetarian || order === hawaiian || order === pepperoni) {
+          return true;
+        } else {
+          return false;
+        }
+      };
 
-console.log(vegetarian, hawaiian, pepperoni);
-
-//2
-
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
-
-//3
-
-const orderName = prompt ("Enter the name of the pizza you want to order today.");
-
-//5
-
-if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
-    const orderQuantity = +prompt (`How many of ${orderName} do you want?`);
-///
-const orderTotal = (pizzaPrice * orderQuantity);
-if (orderQuantity <= 2) {
-    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take 10 minutes.`);
-} else if (orderQuantity  >= 3 && orderQuantity <=5) {
-    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take 15 minutes.`);
-} else {
-    alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take 20 minutes.`);
+// *** Function 2 - Total Cost
+const calculateTotalCost = (quantity, price) => {
+  return quantity * price;
 }
 
-///
-
-
-} else {
-    alert('Select a pizza from the menu');
+// *** Function 3 - Cooking Time
+const calculateCookingTime = (time) => {
+  if (time <= 2) {
+    return 10;
+  } else if (time >= 3 && time <=5) {
+    return 15;
+  } else {
+    return 20;
+  }
 }
 
-//console.log(orderName);
+alert(
+    `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+  );
 
-//console.log(orderQuantity);
+const orderName = prompt(
+    "Enter the name of the pizza you want to order today."
+  );
 
-//4
+if (validateOrderName(orderName) === true) {
+  const orderQuantity = +prompt(`How many of ${orderName} do you want?`);
+  ///
+  const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
+  const cookingTime = calculateCookingTime(orderQuantity);
+  alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`);
+  ///
+} else {
+  alert('Select a pizza from the menu');
+}
 
-//const orderTotal = (pizzaPrice * orderQuantity);
 
-//console.log(orderTotal)
 
-//alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`)
 
+
+
+
+
+
+/// How to create a progress bar: https://www.w3schools.com/howto/howto_js_progressbar.asp
+/// Design progress bar: https://medium.com/@CanvasFlip/best-practices-in-designing-awesome-progress-bars-e051bef10c28 & https://codemyui.com/place-order-progress-bar-animation-steps/ & https://uimovement.com/tag/progress-bar/?page=2
+
+/// How to create a countdown timer: https://www.w3schools.com/howto/howto_js_countdown.asp
 
