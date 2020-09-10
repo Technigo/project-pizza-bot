bd5d3c5f7b4df39358f46ff1a867fd710716793f
Rebkova <rebkova@gmail.com>
2020-09-10 21:35:45 +0200

    Iteration 2 done

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,7 +1,13 @@
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
 
 //Put your Javscript code here:
+
+//test works
+//console.log(`Test if connection HTML-JS works: ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+alert(`Welcome, customer! Happy to serve your pizza. On our menu we have: ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
