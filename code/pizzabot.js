81d3a992ee32e3fa30acea98f892fc2788cfa5f8
Olof Lindahl <olindahl@gmail.com>
2020-09-10 17:37:21 +0200

    Working pizza bot

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,95 +4,57 @@ const pepperoni = "Pepperoni Pizza";
 
 const pizzaPrice = 80;
 
-// alert (
-//  // `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`
-// );
+document.getElementById(
+  "welcome"
+).innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`;
 
-document.getElementById('welcome').innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`
-
-//let orderName = prompt(`Enter the name of the pizza you want to order today`);
 let orderName;
 let orderQuantity;
 
 const validateOrderName = (validOrder) => {
-    if (
+  return (
     validOrder == vegetarian ||
     validOrder == hawaiian ||
     validOrder == pepperoni
-  ) {
-    //console.log(validOrder);
-    //orderQuantity = prompt(`How many ${validOrder} do you want?`);
-  } else {
-    //validOrder = prompt(`Enter the name of a pizza on the menu`);
-    //console.log(validOrder);
-    validateOrderName(validOrder);
-  }
-  return validOrder;
+  );
 };
 
 const calculateCookingTime = (orderQuantity) => {
-    if (orderQuantity >= 1 && orderQuantity <= 3) {
-      return 10;
-    } else if (orderQuantity < 6) {
-      return 15;
-    } else {
-      return 20;
-    }
-  };
+  if (orderQuantity >= 1 && orderQuantity <= 3) {
+    return 10;
+  } else if (orderQuantity < 6) {
+    return 15;
+  } else {
+    return 20;
+  }
+};
 
 const calculateTotalCost = (orderQuantity, pizzaPrice) => {
-    return orderQuantity * pizzaPrice;
+  return orderQuantity * pizzaPrice;
+};
+
+const toggle = () => {
+  return this.classList.toggle("active-summary");
 };
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-    let orderName = document.getElementById('orderName').value;
-    let orderQuantity = document.getElementById('orderQuantity').value;
+  event.preventDefault();
+  let orderName = document.getElementById("orderName").value;
+  let orderQuantity = document.getElementById("orderQuantity").value;
 
-    validateOrderName(orderName);
-
-    document.getElementById('orderSummary').innerHTML =
-  ` Great, I´ll get started on your ${orderName} right away, it will cost ${calculateTotalCost(
-    orderQuantity,
-    pizzaPrice
-  )} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`
-}
-
-
-//orderName = validateOrderName(orderName);
-//console.log(orderName);
-
-/*if (validateOrderName(orderName)) {
-  orderQuantity = prompt(`How many of ${orderName} do you want?`);
-} else {
-  orderName = prompt(`Select a pizza from the menu`);
-  validateOrderName(orderName);
-}*/
-
-/*orderQuantity = prompt(`How many of ${orderName} do you want?`);...
-
-validateOrderName (orderName);
-alert(`Select a pizza from the menu`);*/
-
-// if (
-//   orderName === vegetarian ||
-//   orderName === hawaiian ||
-//   orderName === pepperoni
-// ) {
-//   orderQuantity = prompt(`How many of ${orderName} do you want?`);
-// } else {
-//   alert(`Select a pizza from the menu`);
-// }
-
-/*alert(
-  `Great, I´ll get started on your ${orderName} right away, it will cost ${calculateTotalCost(
-    orderQuantity,
-    pizzaPrice
-  )} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`
-);*/
-
-/*document.getElementById('orderSummary').innerHTML =
-  `Great, I´ll get started on your ${orderName} right away, it will cost ${calculateTotalCost(
-    orderQuantity,
-    pizzaPrice
-  )} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`*/
+  if (validateOrderName(orderName)) {
+    document.getElementById("orderSummary").classList = toggle;
+    document.getElementById(
+      "orderSummary"
+    ).innerHTML = ` Great, I´ll get started on your ${orderName} right away, it will cost ${calculateTotalCost(
+      orderQuantity,
+      pizzaPrice
+    )} kr. The pizza(s) will take ${calculateCookingTime(
+      orderQuantity
+    )} minutes.`;
+  }
+  else {
+    document.getElementById("orderSummary").classList = toggle;
+    document.getElementById("orderSummary").innerHTML = "Please chooose a Pizza from the menu"; 
+  }
+};
