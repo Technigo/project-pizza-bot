e99a9efd38ea2864b61a6560b7e247cc0cd75f3f
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-10 15:50:59 +0200

    Added 3 pizza images, Updated HTML with form elements and other html elements to be used when using innerhtml in javascript. In javascript edited some of the didifferent prompt sentances, deleted validateordername function call and the pizzaisonmenu variable that the function data was inserted into. Replaced the pizzaisonmenu if and else statements with a new one which allows for the onsubmit event to be actioned when run. This still gives a true or false if else statement but get's the value of the pizza the user has entered from what they input in the html input text box. Added a pizzaimage variable which will hopefully allow for us to switch between images shown depending upon the pizza selected. And a new function which is called when the user submits the information from the enteredpizzaname. In CSS updated the styling for the second form and also enabled some display: none css for some elements which will be shown when the javascript function is called.

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,24 +4,21 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80;
 
-// Greeting giving the user the menu
-document.getElementById("greeting").innerHTML = `Hey! Welcome to the pizza bot! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`;
+// Greeting giving the user the pizza choices
+document.getElementById("greeting").innerHTML = `I'm pizza bot &#129302; and I'm here to help you with your order. On our menu we have: <li>${vegetarian}</li><li>${hawaiian}</li><li>${pepperoni}</li>`;
 
-// Enter name of pizza you want to order
-let orderName = document.getElementById("choosePizza").innerHTML = "Choose the name of the pizza you want to order today";
+//Message to the user asking them to choose a pizza from the menu
+document.getElementById("choosePizza").innerHTML = "Ok great. Let's get started with your order! Please write the name of the pizza you want to order:";
 
-//Function compares the paramater chosenPizza is true or false and will return which ever is true
+//Function compares the parameter chosenPizza is true or false and will return which ever is true
 const validateOrderName = (chosenPizza) => {
     if (chosenPizza.toLowerCase() === vegetarian.toLowerCase() || chosenPizza.toLowerCase() === hawaiian.toLowerCase() || chosenPizza.toLowerCase() === pepperoni.toLowerCase()){
-        return true; 
+        return true;
     }
     else { 
         return false;
     }
 };
-
-//Calling the function and in the paramater we define what is in the orderName variable to then tell chosenPizza what pizza has been chosen so then it can check this against what is defined in the if and else statements. The outcome, true or false will be stored on the pizzaIsOnMenu variable.
-let pizzaIsOnMenu = validateOrderName(orderName);
 
 //Function calculating the cost of the pizza based on the pizzaAmount and returning the amount.
 const calculateTotalCost = (orderQuantity, pizzaPrice) => {
@@ -29,6 +26,7 @@ const calculateTotalCost = (orderQuantity, pizzaPrice) => {
     return orderTotal;     
 };
 
+//Function calculating the cooking time based on amount of pizzas ordered
 const calculateCookingTime = (pizzaAmount) => {
     if (pizzaAmount <= 2) {
         return 10;
@@ -39,17 +37,49 @@ const calculateCookingTime = (pizzaAmount) => {
     }
 };
 
-//if statement which tells us if the user has put in a valid pizza name and if true then will create the promt asking them how many they want, which is then stored in the variable pizzaAmount. Otherwsie they will get the alert.
-if (pizzaIsOnMenu === true) {
-    let pizzaAmount = document.getElementById("howmany").innerHTML = `How many of ${orderName} do you want?`;
-    //Calling the function and in the parameter taking the data inside the pizzaAmount and pizzaPrice and using that to put into into the calculateCost function to then be able to do the calculation.
-    let totalPrice = calculateTotalCost(pizzaAmount, pizzaPrice);
-    let cookingTimeMin = calculateCookingTime(pizzaAmount);
-    document.getElementById("finalize").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${totalPrice} kr. The Pizzas will take ${cookingTimeMin}.`;
-} else {
-    document.getElementById("error").innerHTML = "Sorry you have to order something off the menu";
-}
+//New function to be called when the user inputs the pizza they want + press the submit button, which then shows the next prompt of how many they want. If that is not true then the get the wrongPizza message asking them to order a pizza from the menu.
+const enteredPizzaName = (event) => {
+    event.preventDefault();
+    let orderName = document.getElementById("enterPizza").value;
+    let correctPizzaName = validateOrderName(orderName);
 
+    if (correctPizzaName === true) {
+        document.getElementById("howMany").innerHTML = `And how many ${orderName}'s would you like to order?`;
+        document.getElementById("formTwo").style.display = "flex";
+    }
+    else {
+        document.getElementById("wrongPizza").innerHTML = "Please order a pizza from the menu";
+        document.getElementById("orderDifferentPizza").style.display= "flex";
+    }
+};
+
+pizzaImage = (orderName) => { 
+    if (orderName === vegetarian) {
+        document.getElementById("vegPizza").src = "./vegetarian-pizza.jpg";
+    } else if (orderName === pepperoni) {
+        document.getElementById("pepPizza").src = "./pepperoni-pizza.jpg";
+    } else {
+        document.getElementById("hawPizza").src= "./hawaiian-pizza.png";
+    }
+};
+
+
+//New function to be called when the user inputs the amount of pizzas they want + press the submit button, which then shows the final message which clarifies how what kind of pizza they've ordered, how much it'll cost and the amount of minuites it'll take to cook the pizza. 
+const enteredPizzaAmount = (event) => {
+    event.preventDefault();
+    let orderAmount = document.getElementById("enterAmount").value;
+    let correctOrderAmount = calculateCookingTime(orderAmount);
+    let totalPrice = calculateTotalCost(orderAmount, pizzaPrice);
+    let orderName = document.getElementById("enterPizza").value;
+    document.getElementById("finalize").innerHTML = `Thanks for your order! I'll get started on your ${orderName} right away, it will cost ${totalPrice} kr. The Pizzas will take ${correctOrderAmount} minuites.`;
+    document.getElementById("finalOrder").style.display= "flex";
+    document.getElementById("showPizzaPicture").innerHTML = pizzaImage(orderName);
+    
+    // document.getElementById("vegPizza").style.display= "flex";
+};
+
+//Changed the javascript code so it doesn't only prompt or print the information to the DOM. Now it prints certain questions based on user input. 
+//Functions being called when user inputs information and clicks submit. Before javascript was being run based on prompts which automatically waited for user input. But now javascript is being run on demand when an event happens(call back) and for this to work you need to seperate the parts of the programs into functions. And this is why the code has been moved around (refactored).
 
 
 
