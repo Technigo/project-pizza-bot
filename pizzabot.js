bcf220794e604da40b49d1d467b798b42c6539aa
Peggy <64912576+pwangy@users.noreply.github.com>
2020-09-10 18:42:04 +0200

    footer. process and validation of order.

--- pizzabot.js
+++ pizzabot.js
@@ -62,95 +62,22 @@ const calculateCookingTime = (finalOrderQty) => {
   }
 };
 
+const processOrder = (pizzaChoice) => {
+  const finalOrderQty = document.getElementById("orderQty").value;
+    let orderTotal = calculateTotalCost(finalOrderQty, pizzaPrice);
+    let pizzaCookingTime = calculateCookingTime(finalOrderQty);
+    const orderSummary = document.getElementById("confirmation");
+    orderSummary.style.display = "block";
+    document.getElementById("confirmation").innerHTML = `Great! I will get started on your ${pizzaChoice} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`;
+};
+
 const validateOrder = (event) => {
   event.preventDefault(); 
   if (document.getElementById("vegetarian").checked === true) {
-    const finalOrderQty = document.getElementById("orderQty").value;
-    let orderTotal = calculateTotalCost(finalOrderQty, pizzaPrice);
-    let pizzaCookingTime = calculateCookingTime(finalOrderQty);
-    const orderSummary = document.getElementById("confirmation");
-    orderSummary.style.display = "block";
-    document.getElementById(
-  "confirmation"
-).innerHTML = `Great! I will get started on your ${validateOrder} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`;
-    console.log(`Order is: ${finalOrderQty} ${hawaiian}`);
+    processOrder(vegetarian);
   } else if (document.getElementById("hawaiian").checked === true) {
-    const finalOrderQty = document.getElementById("orderQty").value;
-    let orderTotal = calculateTotalCost(finalOrderQty, pizzaPrice);
-    let pizzaCookingTime = calculateCookingTime(finalOrderQty);
-    const orderSummary = document.getElementById("confirmation");
-    orderSummary.style.display = "block";
-    document.getElementById(
-  "confirmation"
-).innerHTML = `Great! I will get started on your ${validateOrder} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`;
-    console.log(`Order is: ${finalOrderQty} ${hawaiian}`);
+    processOrder(hawaiian);
   } else {
-    const finalOrderQty = document.getElementById("orderQty").value;
-    let orderTotal = calculateTotalCost(finalOrderQty, pizzaPrice);
-    let pizzaCookingTime = calculateCookingTime(finalOrderQty);
-    const orderSummary = document.getElementById("confirmation");
-    orderSummary.style.display = "block";
-    document.getElementById(
-  "confirmation"
-).innerHTML = `Great! I will get started on your ${validateOrder} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`;
-    document.getElementById(
-  "confirmation")
-console.log(`Order is: ${finalOrderQty} ${pepperoni}`);
+    processOrder(pepperoni);
   }
 };
-
-
-// let orderTotal = calculateTotalCost(finalOrderQty, pizzaPrice);
-// let pizzaCookingTime = calculateCookingTime(finalOrderQty);
-// document.getElementById(
-//   "confirmation"
-// ).innerHTML = `Great! I will get started on your ${orderName} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`;
-
-// if (validateOrderName(orderName)) {
-//   let orderQty = prompt(`How many of ${orderName} do you want?`);
-  // let orderTotal = calculateTotalCost(orderQty, pizzaPrice);
-  // let pizzaCookingTime = calculateCookingTime(orderQty);
-  // alert(
-  //   `Great! I will get started on your ${orderName} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`
-  // );
-
-
-
-/*- Understand control flow and the use of conditionals statements
-- Be able to write basic functions which take arguments and return values
-- Connect the JavaScript to show visual feedback in the HTML
-
-
-## How to hand in the code 🎯
-- When you’re finished with the project, push your code to GitHub 
-- Navigate to your repo and create a Pull Request into the Technigo repo (Add a link to your deployed project and both of your names.)
-- Wait for the code review from your teachers
-
-
-## Requirements 🧪
-**General Requirements**
-- Contribute by helping others with this project on Stack Overflow.
-- If selected; demo your solution for your team.
-- Code follows Technigo’s code guidelines.
-- Publish your site on Netlify.
-
-**🔵  Blue Level (Minimum Requirements)**
-- Work together in a pair, make sure both of you understand what's happening.
-- Complete all project iterations.
-- Show the final order summary visually by updating the DOM using Javascript.
-
-Make sure you've committed and pushed a version of your project before starting with the intermediary and advanced goals.
-
-
-**🔴  Red Level (Intermediary Goals)**
-- Make an HTML form for the input of the user's name, pizza type, number of pizzas. Choose the form elements that you think fit the purpose best.
-- Show images of the pizza you ordered when you see the final order details.
-- Add a few more pizza types and/or different prices for each pizza type
-
-
-**⚫  Black Level (Advanced Goals)**
-- Create a progress bar indicating how many steps there are to finish the order.
-- Make a little countdown timer that shows up when your order is finished. Starting on 10, 15 or 20 minutes accordingly to the amount of pizzas you ordered.
-- Use an array of objects to store the information about the pizzas
-- Style and structure your HTML/CSS so the user is guided through the ordering process.
-*/
