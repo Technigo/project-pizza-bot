e8246ee0a08dcd0c697d222b90bdc73bd00a868d
MindstormingAB <s.elander@mindstorming.se>
2020-09-10 23:29:27 +0200

    With form input

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -6,13 +6,13 @@ const pizzaPrice = 80
 
 
 // validateOrderName() which should take the orderName variable as an argument and return true or false if the pizza is on the menu or not.
-const validateOrderName = (orderName) => {
-  if ((orderName === hawaiian) || (orderName === vegetarian) || (orderName === pepperoni)) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// const validateOrderName = (orderName) => {
+//   if ((orderName === hawaiian) || (orderName === vegetarian) || (orderName === pepperoni)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 // calculateTotalCost() which takes orderQuantity and pizzaPrice as arguments and returns the total cost for the order.
 const calculationTotalCost = (orderQuantity, pizzaPrice) => {
@@ -30,19 +30,45 @@ const calculateCookingTime = (orderQuantity) => {
   }
 }
 
-// The whole order as a function
-const botOrder = () => {
-  alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
-  const orderName = prompt('Enter the name of the pizza you want to order today.');
-  if (validateOrderName(orderName) === true) {
-    const orderQuantity = prompt(`How many ${orderName}(s) do you want?`);
-    const confirmation = (`Great, I'll get started on your ${orderName}(s) right away, it will cost ${calculationTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`);
-    document.getElementById("confirmation-container").style.display = "flex";
-    document.getElementById('confirmation').innerHTML = confirmation;
-  } else {
-    alert('Select a pizza from the menu');
-    botOrder();
-  }
+
+const greet = (event) => {
+  event.preventDefault();
+  const greeting = (`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`);
+  const enterName = ('What is your name?');
+  document.getElementById('greeting-container').style.display = "flex";
+  document.getElementById('enterName-container').style.display = "flex";
+  document.getElementById('greeting').innerHTML = greeting;
+  document.getElementById('enterName').innerHTML = enterName;
+  document.getElementById('nameForm-container').style.display = "flex";
+  document.getElementById('nameForm').style.display = "block";
 }
 
-botOrder();
+const submitName = (event) => {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const orderName = (`${name}, enter the name of the pizza you want to order today.`);
+  document.getElementById('pizzaName-container').style.display = "flex";
+  document.getElementById('pizzaName').innerHTML = orderName;
+  document.getElementById('pizzaForm-container').style.display = "flex";
+  document.getElementById('pizzaForm').style.display = "block";
+}
+
+const submitPizza = (event) => {
+  event.preventDefault();
+  const orderName = document.getElementById('orderName').value;
+  const pizzaQuantity = (`How many ${orderName}(s) do you want?`);
+  document.getElementById('pizzaQuantity-container').style.display = "flex";
+  document.getElementById('pizzaQuantity').innerHTML = pizzaQuantity;
+  document.getElementById('quantityForm-container').style.display = "flex";
+  document.getElementById('quantityForm').style.display = "block";
+}
+
+const submitQuantity = (event) => {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const orderName = document.getElementById('orderName').value;
+  const orderQuantity = document.getElementById('orderQuantity').value;
+  const confirmation = (`Great ${name}! I'll get started on your ${orderName}(s) right away, it will cost ${calculationTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`);
+  document.getElementById('confirmation-container').style.display = "flex";
+  document.getElementById('confirmation').innerHTML = confirmation;
+}
