684ad6b4f2bea68605428058274a28fd80b41bd3
Therese <Therese@Thereses-MacBook-Air.local>
2020-09-10 08:49:20 +0200

    update Iteration 6

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -14,36 +14,48 @@ const orderPizza = () => {
   );
 
   const validateOrderName = () => {
-    if (orderName === vegetarian || hawaiian || pepperoni) {
+    if (
+      orderName === vegetarian ||
+      orderName === hawaiian ||
+      orderName === pepperoni
+    ) {
       //checks if orderName is true, if orderName false, go to the last else
-      const orderQuantity = prompt(`How many of ${orderName}s do you want?`);
-      //then prompts how many pizzas you want
-      const orderTotal = orderQuantity * pizzaPrice;
-      //calculates the total amount of money
-      const calculateCookingTime = () => {
-        //checks cooking time after the amount of pizzas
-        if (orderQuantity <= 2) {
-          alert("The pizzas will take 10 minutes.");
-        } else if (orderQuantity < 6) {
-          alert("The pizzas will take 15 minutes.");
-        } else {
-          alert("The pizzas will take 20 minutes.");
-        }
-      };
-      alert(
-        `Great I'll get started on your ${orderName}s right away, it will cost ${orderTotal} kr.`
-        //confirms pizzaorder and the cost
-      );
-      calculateCookingTime();
-      //invokes calculateCookingTime function, shows cooking time
+      return true;
     } else {
-      alert("Select a pizza from the menu");
-      /*if orderName false, should prompt again, or loop from start, 
-      must fix this, just realised this doesn't work...aaaaaaaaaaarrrrrghhhhhh */
+      alert("Choose a pizza that's on the menu");
+
+      return false;
     }
+    /*if orderName false, should prompt again, or loop from start,
+     */
   };
-  validateOrderName();
-  //invokes the function validateOrderName, which is nested inside orderPizza
+
+  if (validateOrderName(orderName)) {
+    const orderQuantity = prompt(`How many ${orderName}s do you want?`);
+    //then prompts how many pizzas you want
+    const orderTotal = orderQuantity * pizzaPrice;
+    //calculates the total amount of money
+
+    const calculateCookingTime = () => {
+      //checks cooking time after the amount of pizzas
+      if (orderQuantity <= 2) {
+        alert("The pizzas will take 10 minutes.");
+      } else if (orderQuantity < 6) {
+        alert("The pizzas will take 15 minutes.");
+      } else {
+        alert("The pizzas will take 20 minutes.");
+      }
+    };
+
+    alert(
+      `Great I'll get started on your ${orderName}s right away, it will cost ${orderTotal} kr.`
+    );
+    calculateCookingTime();
+    //invokes calculateCookingTime function, shows cooking time
+    //confirms pizzaorder and the cost
+  } else {
+    orderPizza();
+  }
 };
 
 orderPizza();
