4a765f02c762370296ee380e7a4618c6d9b19269
Katsiez <katarinajovanovic.a@gmail.com>
2020-09-10 19:15:53 +0200

    Our chat bot should now be fully optimized. It takes one of the 3 orders listed and returns a order message, as well as an error message if not found on the menu.

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,26 +1,15 @@
-// GRADIENT BACKGROUND
 
-
-// Iteration 1 - Setting everything up
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
-
-const welcome = () => {
-    orderName = prompt(`Enter the name of the pizza you want to order today.`)
-    validateOrderName(orderName)
-}
-
-const calculateTotalCost = () => {
-    orderQuantity = prompt(`How many of ${orderName} do you want?`)
+const calculateTotalCost = (orderQuantity) => {
     orderTotal = (orderQuantity * pizzaPrice)
 }
 
-const calculateCookingTime = () => {
+const calculateCookingTime = (orderQuantity) => {
     if (orderQuantity > 0 && orderQuantity <= 2) {
         orderTime = 10
     } else if (orderQuantity >= 3 && orderQuantity <= 5) {
@@ -32,20 +21,22 @@ const calculateCookingTime = () => {
 
 const wrongOrder = () => {
     alert(`No such pizza on the menu. Please choose one of the following pizzas: ${vegetarian}, ${hawaiian} or ${pepperoni}`)
-    welcome()
 }
 
-let validateOrderName = () => {
+let validateOrderName = (orderName, orderQuantity) => {
+    event.preventDefault();
     if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
-        calculateTotalCost()
-        calculateCookingTime()
-        return alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and it will take ${orderTime} minutes.`)
+        calculateTotalCost(orderQuantity)
+        calculateCookingTime(orderQuantity)
+        document.getElementById("final").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and take ${orderTime} minutes.`
     } else {
-        return wrongOrder()
+        document.getElementById("final").innerHTML = `No such pizza on the menu. Please choose one of the following pizzas: ${vegetarian}, ${hawaiian} or ${pepperoni}`
     }
 }
 
-welcome()
+
+
+
 
 
 
