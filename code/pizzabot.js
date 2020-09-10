914ad5dfd26cc91653dc4f66d920b6267e20acf1
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-09-10 09:23:49 +0200

    Blue level completed

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -9,10 +9,9 @@ console.log("Vegetarian Pizza", "Hawaiian Pizza","Pepperoni Pizza");
 //Put your Javscript code here:
 
 /*iteration 2*/ 
+//alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
 
 
-
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
 
 /*iteration 3 and 5*/ 
 /* const orderName = prompt("Enter the name of the pizza you want to order today.");
@@ -31,26 +30,35 @@ if ((orderName === vegetarian) || (orderName === hawaiian) || (orderName === pep
 }
  */
 
-/*Iteration 6*/
 
-/* Function for validateOrderName */
-const orderName = prompt("Enter the name of the pizza you want to order today.");
+
+
+//Iteration 2 
+// alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`)
+
+const orderPizza = (event) => {
+    event.preventDefault();
+    alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+ 
+
+// Iteration 6 - Add functions
+
+const orderName = prompt('Enter the name of the pizza you want to order today.');
+
 const validateOrderName = (orderName) => {
-    if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni){ 
-    return true; 
-    } else {
+  if ((orderName === hawaiian) || (orderName === vegetarian) || (orderName === pepperoni)) {
+    return true;
+  } else {
     return false;
-    }
   }
-
-    
-/* Function for calculateTotalCost */
-const calculateTotalCost = (orderQuantity, pizzaPrice) => {
-return orderQuantity * pizzaPrice;
-
 }
 
-/* Function for calculateCookingTime */
+
+const calculationTotalCost = (orderQuantity, pizzaPrice) => {
+  return orderQuantity * pizzaPrice;
+}
+
+
 const calculateCookingTime = (orderQuantity) => {
   if (orderQuantity <= 2) {
     return 10;
@@ -59,16 +67,17 @@ const calculateCookingTime = (orderQuantity) => {
   } else {
     return 20;
   }
-
 }
 
 if (validateOrderName(orderName) === true) {
   const orderQuantity = prompt(`How many ${orderName}(s) do you want?`);
-  alert(`Great, I'll get started on your ${orderName} pizza(s) right away, it will cost ${calculationTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`);
-  } else {
+ /*  alert(`Great, I'll get started on your ${orderName}(s) right away, it will cost ${calculationTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`); */
+  document.getElementById("chat").innerHTML= (`Great I'll get started with your ${orderName}(s) right away, it will cost ${calculationTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes."`);
+} else {
   alert('Select a pizza from the menu');
-  } 
-    
+}
+
+}
 
 
 
@@ -81,10 +90,7 @@ const orderPizza = () => {
 }
 */
 
-const orderPizza = (event) => {
-  event.preventDefault();
-  alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
-}
 
 
-document.getElementById("endMessage").textContent = "Thank you for ordering from our restaurant.Welcome back soon!"
+
+//document.getElementById("endMessage").textContent = "Thank you for ordering from our restaurant.Welcome back soon!"
