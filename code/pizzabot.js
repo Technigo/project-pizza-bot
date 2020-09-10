4d28d40d7217433ef064fb2264904695d7a0aa53
Gabbi-89 <gabriellabolin@hotmail.com>
2020-09-10 16:53:46 +0200

    End of Thursday work. Red level done plus some things from black level.

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,7 +4,7 @@ const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 const kebab = "Kebab Pizza"
 
-const pizzaPrice = 80
+let pizzaPrice = 80
 
 // Welcome message - Pizza-chat box 2
 document.getElementById("welcome").innerHTML = `On our menu we have ${vegetarian}, ${hawaiian}, ${pepperoni} and ${kebab}.`
@@ -27,12 +27,24 @@ let displayOrderQuantityBox = () => {
 let orderPizza = (name) => {
     orderName = name
     document.getElementById("howMany").innerHTML = `OK, how many ${orderName}(s) do you want?`
+    
+    // Show progress-bar when clicking on a pizza-sort
+    const progressBar = document.getElementById("progressBar")
+    progressBar.classList.remove("invisible")
+    progressBar.classList.add("visible")
+
+    // Push header-section down when progress-bar is shown
+    const headerBox = document.querySelector("header")
+    headerBox.style.marginTop = "150px"
+    
+    // Add pizza-name + first progress-box to progress-bar
+    document.getElementById("topBarPizza").innerHTML = `${orderName}`
+    document.getElementById("progressOne").style.background = "#01DC3E"
+
     displayOrderQuantityBox()
 }
 
 // Counting-function to select amount of pizzas
-
-
 let quantityFunction = (event) => {
     event.preventDefault()
 
@@ -45,9 +57,23 @@ let quantityFunction = (event) => {
     // Total cost-function
     let orderTotal = 1
     let calculateTotalCost = () => {
+        
+        // Conditional statement given other price for kebab pizza
+        let otherPrice = () => {
+            if (orderName === kebab)
+            {
+                pizzaPrice = 100
+            } 
+            else {
+                pizzaPrice = 80
+            }
+        }
+        
+        otherPrice() 
+
         orderTotal = orderQuantity * pizzaPrice
     }
-    
+
     // Total cooking-time-function
     let cookingTime = 1
 
@@ -60,25 +86,53 @@ let quantityFunction = (event) => {
             cookingTime = 20
         }
     }
+    
     calculateTotalCost()
     calculateCookingTime()
 
-
     // Confirmation message
+    document.getElementById("topBarCount").innerHTML = `${orderQuantity} x`
+    document.getElementById("progressTwo").style.background = "#01DC3E"
     document.getElementById("confirmation").innerHTML = `Great, I'll get started on your ${orderName}(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`
-
+    
     confirmationBox.classList.remove("invisible")
     confirmationBox.classList.add("visible")
 
+    //Count-down timer gets visible
+    timerBox.classList.remove("invisible")
+    timerBox.classList.add("visible")
+
+    //Order Summary   
     document.getElementById("count").innerHTML = `${orderQuantity}`
     document.getElementById("pizza").innerHTML = `${orderName}`
     document.getElementById("price").innerHTML = `${pizzaPrice} kr`
     document.getElementById("total").innerHTML = `Total: ${orderTotal} kr`
-
+    document.getElementById("topBarTotal").innerHTML = ` = ${orderTotal} kr`
+    
+    //Order Summary gets visible
     const orderSummaryBox = document.getElementById("orderSummaryBox")
     orderSummaryBox.classList.remove("invisible")
     orderSummaryBox.classList.add("visible")
 
- 
+    //Count-down timer function
+    let startingMinutes = cookingTime
+    let time = startingMinutes * 60;
+
+    const countdownEl = document.getElementById("time");
+
+    setInterval(updateCountdown, 1000);
+
+    function updateCountdown() { // Det funkar inte att skriva om denna till så vi lärt oss... 
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        countdownEl.innerHTML = `${minutes}:${seconds}`;
+        time--;
+
+    }
 }
+
+
 
