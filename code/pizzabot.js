d64ac89ca719361f3fee5aafd3e32929c4be745a
caroline birgersson <caroline@birgerochblom.se>
2020-09-11 16:37:11 +0200

    Iteration 5

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -10,6 +10,7 @@ console.log(vegetarian);
 console.log(hawaiian);
 console.log(pepperoni);
 
+
 //Greeting:
 
 const x = "vegetarian";
@@ -18,43 +19,37 @@ const z = "pepperoni";
 
 alert(`Hey! Happy to serve your pizza. On our menu we have ${x}, ${y} and ${z}.`);
 
-
 //Ask the customer:
+const orderName = prompt(
+  "Enter the name of the pizza you want to order today.");
 
-const orderName () =>  
-    prompt("Enter the name of the pizza you want to order today.")
-    if (orderName !== x || orderName !== y || orderName !== z)
-    alert("choose a pizza on the menu") 
-    return true
-}
-
-
-//Add conditionals: //Checking that the pizza is on the menu:
-
-
-orderQuantity = prompt(`How many of ${orderName} do you want?`);
-const orderTotal = orderQuantity * pizzaPrice
-console.log(orderName, orderQuantity);
-
-
-
-//Finalize the order:
-const pizza10 = "The pizza will take 10 minutes";
-const pizza15 = "The pizza will take 15 minutes";
-const pizza20 = "The pizza will take 20 minutes";
-
-
-orderQuantity = alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. ${pizza10} ${pizza15} ${pizza20}`);
-
-const pizzaTime = (orderQuantity) => {
-}
-    orderName = document.getElementById("pizzas").value
-    if (orderQuantity <= 2) {
-      cookingTime = "10 minutes"
-    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-      cookingTime = "15 minutes"
+//ValidateOrder //CookingTime //calculateOrder //Finalize the order
+const validateOrderName = (order) => {
+  if (order === x || order === y || order === z) {
+    return true;
     } else {
-      cookingTime = "20 minutes"
+      alert("Please, check the menu")
+      return false;
     }
+  };
 
-console.log(orderTotal);
+const calculateTotalCost = (quantity, price) => {
+   return quantity * price;
+  }
+
+const calculateCookingTime = (time) => {
+    if (time <= 2) {
+      return 10;
+    } else if (time >= 3 && time <=5) {
+      return 15;
+    } else {
+      return 20;
+    }
+  }
+
+if (validateOrderName(orderName) === true) {
+const orderQuantity = +prompt(`How many of ${orderName} do you want?`);
+const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
+const cookingTime = calculateCookingTime(orderQuantity);
+alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`);
+}
