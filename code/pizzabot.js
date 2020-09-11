364344b29a1dd5d34b15403a91e597f4272fab75
Evelina Berg <evelina.berg@outlook.com>
2020-09-11 19:16:09 +0200

    Cleaning up the JS code

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,4 +1,4 @@
-//1) Defenition of variables 
+// 1. Definition of variables 
 
 const vegetarian = "Vegetarian"
 const hawaiian = "Hawaiian"
@@ -6,29 +6,29 @@ const pepperoni = "Pepperoni"
 const pizzaPrice = 80
 
 
-//Functions
+// 2. Functions
 
+// Function for validating ordername
 const validateOrderName = (orderName) => {
     if (orderName.toLowerCase() === vegetarian.toLowerCase()) {
         return true;
-
     } else if (orderName.toLowerCase() === hawaiian.toLowerCase()) {
         return true;
-    
     } else if (orderName.toLowerCase() === pepperoni.toLowerCase()) {
         return true;
-    
     } else {
         alert('Select a pizza from the menu!');
         return false;
     }
 };
 
+// Function for calculating total cost
 const calculateTotalCost = (orderQuantity, pizzaPrice) => {
     let orderTotal = orderQuantity * pizzaPrice;
     return orderTotal;
-}
+};
 
+// Function for calculating total cooking time
 const calculateCookingTime = (orderQuantity) => {
     let orderTime = 0; 
     if (orderQuantity <= 2) {
@@ -43,38 +43,44 @@ const calculateCookingTime = (orderQuantity) => {
         orderTime = 20;
         return orderTime;
     }
-}
+};
 
-//Welcome message 
+//Function for asking what kind of pizza they would like to order 
+const askPizzas = () => {
+    const orderName = `On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}. Choose which pizza you want.`
+    document.getElementById("pizzaName").innerHTML = orderName;
+};
+
+ 
+// Function to ask how many pizzas they would like to order 
+const askNumber = () => {
+    const orderQuantity = `How many Pizzas do you want?`;
+    document.getElementById("pizzaQuantity").innerHTML = orderQuantity;
+};
+
+// Function for input value from pizza name
+const getOrderName = () => {
+    return document.getElementById('pizzaNameInput').value;
+};
+
+// Function for input value from pizza quantity
+const getOrderQuantity = () => {
+    return document.getElementById('pizzaQuantityInput').value;
+};
+
+// The chatbot / form
+
+// Welcome message 
 const firstMessage = document.querySelector('.welcome'); 
 const welcomeMessage = `Hey! Happy to serve your pizza!`;
 firstMessage.innerHTML = `<div>${welcomeMessage}</div>`
 console.log(welcomeMessage);
 
-//Ask what pizza they would like to order 
-const askPizzas = () => {
-    const orderName = `On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}. Choose which pizza you want.`
-    document.getElementById("pizzaName").innerHTML = orderName;
-  }
-  askPizzas()
-
-//Ask how many pizzas they would like to order 
-const askNumber = () => {
-    const orderQuantity = `How many Pizzas do you want?`;
-    document.getElementById("pizzaQuantity").innerHTML = orderQuantity;
-  }
+// Form questions
+askPizzas();
 askNumber();
 
-console.log(askPizzas);
-
-const getOrderName = () => {
-    return document.getElementById('pizzaNameInput').value;
-};
-
-const getOrderQuantity = () => {
-    return document.getElementById('pizzaQuantityInput').value;
-};
-
+// Handle user input
 const handleOrder = (event) => {
   event.preventDefault();
   const orderName = getOrderName();
@@ -83,7 +89,7 @@ const handleOrder = (event) => {
   if (isValidOrder === true) {
     const isTotalCost = calculateTotalCost(orderQuantity, pizzaPrice);
     const isOrderTime = calculateCookingTime(orderQuantity);
-    const finalOrderMessage = `Great, I'll get started on your ${orderName} right away, it will cost ${isTotalCost} kr. The pizzas will take ${isOrderTime} minutes.`;
+    const finalOrderMessage = `Great, I'll get started on your ${orderName} right away! It will cost ${isTotalCost} kr. The pizzas will take ${isOrderTime} minutes.`;
     document.getElementById("finalOrderMessage").innerHTML = finalOrderMessage;
   } else {
     return false;
