1cd3e2a37732bb643f2489360f13875d012d4ab9
Emelie Svensson <emeliesv@live.se>
2020-09-11 12:59:49 +0200

    Edited html teeny tiny bit

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,60 +1,60 @@
-
 const vegetarian = "vegetarian pizza";
 const hawaiian = "hawaiian pizza";
 const pepperoni = "pepperoni pizza";
 const margaritha = "margaritha pizza";
-const kebab = "kebab pizza"
-const fusion = "fusion pizza"
+const kebab = "kebab pizza";
+const fusion = "fusion pizza";
 
 const welcomeMessage = (event) => {
-  event.preventDefault()
+  event.preventDefault();
   const username = document.getElementById("name").value;
   showMessageBox(username);
 };
 
 const showMessageBox = (username) => {
-  document.getElementById("message").innerHTML = `Hey ${username}! Let's start our order! Select a pizza from our menu:`;
-  showPizzaMenu()
+  document.getElementById(
+    "message"
+  ).innerHTML = `Hey ${username}! Let's start our order! Select a pizza from our menu:`;
+  showPizzaMenu();
 };
 
 const showPizzaMenu = () => {
-  document.getElementById("pizzaMenu").classList.remove("hidden")
-  
-}
+  document.getElementById("pizzaMenu").classList.remove("hidden");
+};
 
 const validateOrderName = (selectedPizza) => {
-  let selectedPizzaLower = selectedPizza.toLowerCase()
-    return selectedPizzaLower === vegetarian 
-    || selectedPizzaLower === hawaiian 
-    || selectedPizzaLower === pepperoni
-    || selectedPizzaLower === fusion
-    || selectedPizzaLower === margaritha
-    || selectedPizzaLower === kebab
-}
+  let selectedPizzaLower = selectedPizza.toLowerCase();
+  return (
+    selectedPizzaLower === vegetarian ||
+    selectedPizzaLower === hawaiian ||
+    selectedPizzaLower === pepperoni ||
+    selectedPizzaLower === fusion ||
+    selectedPizzaLower === margaritha ||
+    selectedPizzaLower === kebab
+  );
+};
 
 const getPizzaPrice = (selectedPizza) => {
-  if (selectedPizza = "margaritha"){
-    return 65
-  } else if (selectedPizza = "kebab") {
-    return 85
-  } else if (selectedPizza = "fusion") {
-    return 120
+  if ((selectedPizza = "margaritha")) {
+    return 65;
+  } else if ((selectedPizza = "kebab")) {
+    return 85;
+  } else if ((selectedPizza = "fusion")) {
+    return 120;
   } else {
-    return 80
+    return 80;
   }
-}
+};
 
 const showQuantity = () => {
-  
-  const selectedPizza = document.getElementById("selectedPizza").value
-  let validation =  validateOrderName(selectedPizza)
+  const selectedPizza = document.getElementById("selectedPizza").value; //add .toLowerCase?
+  let validation = validateOrderName(selectedPizza);
   if (validation) {
-    document.getElementById("quantity").classList.remove("hidden")
+    document.getElementById("quantity").classList.remove("hidden");
   } else {
-    document.getElementById("errorMessage").classList.remove("hidden")
+    document.getElementById("errorMessage").classList.remove("hidden");
   }
-
-}
+};
 
 const calculateCookingTime = (orderQuantity) => {
   if (orderQuantity > 0 && orderQuantity <= 2) {
@@ -67,35 +67,37 @@ const calculateCookingTime = (orderQuantity) => {
 };
 
 const showPizzaImage = (selectedPizza) => {
-  if (selectedPizza === vegetarian){
-    document.getElementById("pizzaImg").classList.toggle("img-vegetarian")
-  } else if (selectedPizza === hawaiian){
-    document.getElementById("pizzaImg").classList.toggle("img-hawaiian")
-  } else if (selectedPizza === pepperoni){
-    document.getElementById("pizzaImg").classList.toggle("img-pepperoni")
-  } else if (selectedPizza === fusion){
-    document.getElementById("pizzaImg").classList.toggle("img-fusion")
+  if (selectedPizza === vegetarian) {
+    document.getElementById("pizzaImg").classList.toggle("img-vegetarian");
+  } else if (selectedPizza === hawaiian) {
+    document.getElementById("pizzaImg").classList.toggle("img-hawaiian");
+  } else if (selectedPizza === pepperoni) {
+    document.getElementById("pizzaImg").classList.toggle("img-pepperoni");
+  } else if (selectedPizza === fusion) {
+    document.getElementById("pizzaImg").classList.toggle("img-fusion");
   } else if (selectedPizza === margaritha) {
-    document.getElementById("pizzaImg").classList.toggle("img-margaritha")
-  } else{
-    document.getElementById("pizzaImg").classList.toggle("img-kebab")
+    document.getElementById("pizzaImg").classList.toggle("img-margaritha");
+  } else {
+    document.getElementById("pizzaImg").classList.toggle("img-kebab");
   }
-  
-}
+};
 
 const showTotalOrder = () => {
-  document.getElementById("totalOrder").classList.remove("hidden")
-  const pizzaQuantity = document.getElementById("pizzaQuantity").value
-  const cookingTime = calculateCookingTime(pizzaQuantity)
-  const selectedPizza = document.getElementById("selectedPizza").value.toLowerCase()
-  const totalCost = getPizzaPrice(selectedPizza) * pizzaQuantity
-  showPizzaImage(selectedPizza)
-  document.getElementById("finalOrderMessage").innerHTML = `I'll get started on your ${selectedPizza}(s), It will cost you ${totalCost} kr. The pizza(s) will take ${cookingTime} minutes.`
-  
-}
+  document.getElementById("totalOrder").classList.remove("hidden");
+  const pizzaQuantity = document.getElementById("pizzaQuantity").value;
+  const cookingTime = calculateCookingTime(pizzaQuantity);
+  const selectedPizza = document
+    .getElementById("selectedPizza")
+    .value.toLowerCase();
+  const totalCost = getPizzaPrice(selectedPizza) * pizzaQuantity;
+  showPizzaImage(selectedPizza);
+  document.getElementById(
+    "finalOrderMessage"
+  ).innerHTML = `I'll get started on your ${selectedPizza}(s), It will cost you ${totalCost} kr. The pizza(s) will take ${cookingTime} minutes.`;
+};
 
 const hide = () => {
-  document.getElementById("totalOrder").classList.add("hidden")
-  document.getElementById("quantity").classList.add("hidden")
-  document.getElementById("pizzaMenu").classList.add("hidden")
-}
+  document.getElementById("totalOrder").classList.add("hidden");
+  document.getElementById("quantity").classList.add("hidden");
+  document.getElementById("pizzaMenu").classList.add("hidden");
+};
