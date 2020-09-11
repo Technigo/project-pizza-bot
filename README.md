496b6f3e6d4e12255348b3de7e0ec31d4964c169
ClaireCaudwell <clairecaudwell@gmail.com>
2020-09-11 08:11:07 +0200

    Added new pizza image. Updated the readme, Added a robot emoji which will act as the pizzabot robot. Added the pizza images in html. In javascript we added the pixture variables, where each one contained the image that is defined in the innerhtml. We also added a function and if else conditional which assigned an image to the specific pizza types if chosen by the user. Also added the plural  variable and if statement which helps in our sentances if the user orders one or more pizza.

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # Pizza Bot
 
-Replace this readme with your own information about your project. 
-
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The project was to practice javascript to create an interactive pizzabot and make it work inside the DOM. 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+1. We started by reading through the project and then doing the iteration 1, 2 3 and 4 which we found the easiest. 
 
+2. Then we started to add the conditional statements. The first one where you are meant to create an if else statement; if the user inputs a pizza that is on the menu then that will be true and if not then it'll be false. 
+
+The second if, else if and else statements felt understandable. 
+
+3. Then we got really stuck on the functions. I think this was because we didn't really understand how they work and what arguments to write in the brackets. As well as how to call these and then use/store the information that was created when the function was run. After a bit of help and some googaling we understood better how we were meant to use these. We also created two big functions where all the other small functions were called based on user input and if it was true or false. 
+
+4. Once we go the functions to work together we started to edit the prompts and alerts so we couls view the information in the DOM. We used the document.getElementById().innerhtml to connect both the javascript sections to the html. 
+
+In terms of planning our time and working together, we tried to be methodical with the brief and the iterations. We tried to navigate and drive in intervals. If one if us didnät understand then we tried to explain it to, so we both understood. 
+
+For the end result we managed to get the form functions and onsubmit attribute of the form work inline with the last two functions. This meant that when the user inputted information and submitted with the submit button it made the next form section appear. 
+
+If we had more time it would be nice to make the first greeting sentance from the pizzabot to load with the page, but then you would need to interact with that statement in order to get up the first form section where it asks for you to input your username and input the username you want to order. Also it would be nice to add some animations so you could simulate the pizza bot acting more like a robot that is asking questions with the user can interact with. 
+
+We had some help from various people, as this task was super hard. But even though we found it hard it feels like now that we understand more about variables, if else conditionals and functions. 
 ## View it live
 
 Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+
+
--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,16 +1,29 @@
+//Variables defining data
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80;
 
-// Greeting giving the user the pizza choices
-document.getElementById("greeting").innerHTML = `I'm pizza bot &#129302; and I'm here to help you with your order. On our menu we have: <li>${vegetarian}</li><li>${hawaiian}</li><li>${pepperoni}</li>`;
+const vegPic = document.getElementById("vegPizza");
+const pepPic = document.getElementById("pepPizza");
+const hawPic = document.getElementById("hawPizza");
+
+//--------------------------------------------------------------------------//
+
+//Sentances showing in html via the getElementById()
+// Greeting to the user from the pizzabot
+document.getElementById("greeting").innerHTML = `&#129302 Hi! Welcome to Claire & Claudia's pizzeria. I'm pizza bot and I'm here to help you with your order.`;
+
+//Showing the menu
+document.getElementById("menu").innerHTML = `On the menu we have: <li>${vegetarian}: ${pizzaPrice}kr</li> <li>${hawaiian}: ${pizzaPrice}kr</li> <li>${pepperoni}: ${pizzaPrice}kr</li>`;
 
 //Message to the user asking them to choose a pizza from the menu
-document.getElementById("choosePizza").innerHTML = "Ok great. Let's get started with your order! Please write the name of the pizza you want to order:";
+document.getElementById("choosePizza").innerHTML = "&#129302 Ok great. Let's get started with your order! Please write the name of the pizza you want to order:";
 
-//Function compares the parameter chosenPizza is true or false and will return which ever is true
+//--------------------------------------------------------------------------//
+
+//Function compares if the parameter chosenPizza is true or false. Will return true if user inputs correct pizza name, otherwise will show message which is called further down in enteredPizzaValue
 const validateOrderName = (chosenPizza) => {
     if (chosenPizza.toLowerCase() === vegetarian.toLowerCase() || chosenPizza.toLowerCase() === hawaiian.toLowerCase() || chosenPizza.toLowerCase() === pepperoni.toLowerCase()){
         return true;
@@ -20,7 +33,7 @@ const validateOrderName = (chosenPizza) => {
     }
 };
 
-//Function calculating the cost of the pizza based on the pizzaAmount and returning the amount.
+//Function calculating the cost of the pizza based on the pizzaAmount and pizzaPrice. Returnsg the amount when function is called further down
 const calculateTotalCost = (orderQuantity, pizzaPrice) => {
     const orderTotal = (orderQuantity * pizzaPrice);
     return orderTotal;     
@@ -37,49 +50,66 @@ const calculateCookingTime = (pizzaAmount) => {
     }
 };
 
-//New function to be called when the user inputs the pizza they want + press the submit button, which then shows the next prompt of how many they want. If that is not true then the get the wrongPizza message asking them to order a pizza from the menu.
+//Function that assigns an image to the type of pizza ordered
+pizzaImage = (orderName) => { 
+    console.log(orderName);
+    if (orderName.toLowerCase() === vegetarian.toLowerCase()) {
+        return vegPic;
+    } else if (orderName.toLowerCase() === pepperoni.toLowerCase()) {
+        return pepPic;
+    } else {
+        return hawPic;
+    }
+};
+
+//--------------------------------------------------------------------------//
+
+//Function which is connected to the first form in html inside the onsubmit() attribute of the form. When the user inputs the username and the pizza they want and presses the submit button, if true (i.e. if pizza name matches what's on the menu), it runs the code and also shows the next prompt of how many pizzas they want. If that is not true then the they get the wrongPizza message asking them to order a pizza from the menu.
 const enteredPizzaName = (event) => {
     event.preventDefault();
     let orderName = document.getElementById("enterPizza").value;
     let correctPizzaName = validateOrderName(orderName);
 
     if (correctPizzaName === true) {
-        document.getElementById("howMany").innerHTML = `And how many ${orderName}'s would you like to order?`;
+        document.getElementById("orderDifferentPizza").style.display= "none";
+        document.getElementById("howMany").innerHTML = `&#129302 And how many ${orderName}'s would you like to order?`;
         document.getElementById("formTwo").style.display = "flex";
+        // document.getElementById("formOne").submit();
+        //The second form (where the user will input how many pizzas they want) will be show if true as we make that element in html to display flex. Before this time it's defined as display: none is css.
     }
     else {
-        document.getElementById("wrongPizza").innerHTML = "Please order a pizza from the menu";
+        document.getElementById("wrongPizza").innerHTML = "&#129302 Sorry that pizza isn't on the menu. Please order a pizza from the menu";
         document.getElementById("orderDifferentPizza").style.display= "flex";
     }
 };
 
-pizzaImage = (orderName) => { 
-    if (orderName === vegetarian) {
-        document.getElementById("vegPizza").src = "./vegetarian-pizza.jpg";
-    } else if (orderName === pepperoni) {
-        document.getElementById("pepPizza").src = "./pepperoni-pizza.jpg";
-    } else {
-        document.getElementById("hawPizza").src= "./hawaiian-pizza.png";
-    }
-};
-
-
-//New function to be called when the user inputs the amount of pizzas they want + press the submit button, which then shows the final message which clarifies how what kind of pizza they've ordered, how much it'll cost and the amount of minuites it'll take to cook the pizza. 
+//Function that is connected to the second form in html inside the onsubmit() attribute in the form. When user inputs the amount of pizzas they want and  press the submit button, the code inside the function is run. Which is the final order message clarifying all the details of their order and an image of the pizza they've ordered.   
 const enteredPizzaAmount = (event) => {
-    event.preventDefault();
-    let orderAmount = document.getElementById("enterAmount").value;
+    event.preventDefault(); //Stops the page from refreshing when user presses submit button.
+    let orderAmount = document.getElementById("enterAmount").value; //Taking the value (the amount) the user has inputted in the DOM for the amount of pizzas they want to order and put this inside a function to then use to calculate the cooking time below. 
     let correctOrderAmount = calculateCookingTime(orderAmount);
     let totalPrice = calculateTotalCost(orderAmount, pizzaPrice);
     let orderName = document.getElementById("enterPizza").value;
-    document.getElementById("finalize").innerHTML = `Thanks for your order! I'll get started on your ${orderName} right away, it will cost ${totalPrice} kr. The Pizzas will take ${correctOrderAmount} minuites.`;
+    let pizzaImages = pizzaImage(orderName);
+    document.getElementById("formTwo").style.display = "flex";
+    // document.getElementById("formTwo").submit(); 
+
+    //Defined variable and if statement which helps to change if the pizza if plural or not in the final order statement.
+    let plural = "'s";
+    if (orderAmount === "1") {
+        plural = "";
+    }
+
+    document.getElementById("finalize").innerHTML = `&#129302 Thanks for your order! I'll get started on your ${orderName}${plural} right away, it will cost ${totalPrice}kr. The Pizza${plural} will take ${correctOrderAmount} minuites to cook.`;
+
+    //Changes the display of the final order statement and the correct image of the pizza from none to flex. In css this is defined as nonw. 
     document.getElementById("finalOrder").style.display= "flex";
-    document.getElementById("showPizzaPicture").innerHTML = pizzaImage(orderName);
-    
-    // document.getElementById("vegPizza").style.display= "flex";
+    pizzaImages.style.display = "flex";
 };
 
 //Changed the javascript code so it doesn't only prompt or print the information to the DOM. Now it prints certain questions based on user input. 
 //Functions being called when user inputs information and clicks submit. Before javascript was being run based on prompts which automatically waited for user input. But now javascript is being run on demand when an event happens(call back) and for this to work you need to seperate the parts of the programs into functions. And this is why the code has been moved around (refactored).
+
 
 
 
