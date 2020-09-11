0400bc421e24af3468efb097de219b4fbd9bd7b7
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2020-09-11 16:15:36 +0200

    Created form and replaced prompts with HTML elements and styled the site

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -2,7 +2,7 @@ const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
 
-
+const pizzaPrice = 80;
 
 //Put your Javscript code here:
 console.log(vegetarian,hawaiian,pepperoni);
@@ -35,14 +35,17 @@ const calculateTotalCost = (orderQuantity, pizzaPrice) => {
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    const pizzaPrice = 80
-    const orderName = prompt('Enter the name of the pizza you want to order today.');
-    const orderQuantity = prompt(`How many of ${orderName} do you want?`);
-    const cookingTime = calculateCookingTime(orderQuantity);
-    const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
+    const orderName = document.getElementById("pizzaName").value
+        /*prompt('Enter the name of the pizza you want to order today.');*/
+    
     validateOrderName(orderName); 
     if (validateOrderName(orderName) === true) {
+        const orderQuantity = document.getElementById("pizzaQuantity").value;
+        /*prompt(`How many of ${orderName} do you want?`);*/
+        const cookingTime = calculateCookingTime(orderQuantity);
+        const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);  
         document.getElementById("orderConfirmation").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and the pizzas will take ${cookingTime} minutes`; 
+      
     } else {
         document.getElementById("orderConfirmation").innerHTML = `Sorry, we don't have that pizza. Choose a pizza from the menu`;
     }
