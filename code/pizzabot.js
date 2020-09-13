d4623c9cfa24e4ff5e4f8101b3edaf395a0df5b1
Amanda Flodström <amandaflodstroem@gmail.com>
2020-09-13 10:33:46 +0200

    Changed form to select element

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,13 +4,10 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80
 
-//Put your Javscript code here:
 
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
 
 document.getElementById("menu").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`
-
-//alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, ${pepperoni}.`)
 
 
 const calculateTotalCost = (orderQuantity) => {
@@ -44,23 +41,7 @@ document.getElementById("submit").addEventListener("click", handleClick)
 
 
 
-// const promptOrderName = () => {
-//   orderName = prompt("Enter the name of the pizza you want to order today.")
-//   validateOrderName()
-// }
 
-
-
-// promptOrderName()
-
-
-// const orderQuantity = prompt(`How many of ${orderName} do you want?`)
-// let orderTotal
-
-
-
-
-//alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take ${cookingTime}.`)
 
 
 
