b630f171ac18027e2ca90a1dfbb4e213df583a47
Amanda Flodström <amandaflodstroem@gmail.com>
2020-09-13 11:19:22 +0200

    last push

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,13 +1,19 @@
 const vegetarian = "Vegetarian Pizza"
 const hawaiian = "Hawaiian Pizza"
 const pepperoni = "Pepperoni Pizza"
+const margherita = "Margherita Pizza"
 
 const pizzaPrice = 80
+
+const pizzaVegImg = document.getElementById("veg")
+const pizzaHawImg = document.getElementById("haw")
+const pizzaPepImg = document.getElementById("pep")
+const pizzaMarImg = document.getElementById("mar")
 
 
 console.log(vegetarian, hawaiian, pepperoni, pizzaPrice)
 
-document.getElementById("menu").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`
+document.getElementById("menu").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, ${pepperoni}, and ${margherita}.`
 
 
 const calculateTotalCost = (orderQuantity) => {
@@ -35,6 +41,17 @@ const handleClick = () => {
   const cookingTime = calculateCookingTime(orderQuantity)
 
   document.getElementById("message").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The Pizza will take ${cookingTime}.`
+
+  if (orderName === "Vegetarian Pizza") {
+    pizzaVegImg.setAttribute("style", "display:block;");
+  } else if (orderName === "Hawaiian Pizza") {
+    pizzaHawImg.setAttribute("style", "display:block;")
+  } else if (orderName === "Pepperoni Pizza") {
+    pizzaPepImg.setAttribute("style", "display:block;")
+  } else if (orderName === "Margherita Pizza") {
+    pizzaMarImg.setAttribute("style", "display:block;")
+  }
+
 }
 
 document.getElementById("submit").addEventListener("click", handleClick)
