5417ca6b3b175f9385446afef99e4c79829a0ac5
Peggy <64912576+pwangy@users.noreply.github.com>
2020-09-15 21:23:54 +0200

    working through all code suggestions

--- blue_level/pizzabot.js
+++ blue_level/pizzabot.js
@@ -3,52 +3,41 @@ const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
 const pizzaPrice = 80;
 
-const validateOrderName = (orderName) => {
+const validateOrderName = orderName => {
   if (
     orderName === "vegetarian" ||
     orderName === "pepperoni" ||
     orderName === "hawaiian"
   ) {
     return true;
-  } else {
+    } 
     return false;
-  }
 };
 
-const calculateTotalCost = (orderQty, pizzaPrice) => {
-  return orderQty * pizzaPrice;
-};
+const calculateTotalCost = (orderQty, pizzaPrice) => orderQty * pizzaPrice;
 
-const calculateCookingTime = (orderQty) => {
-  if (orderQty <= 2) {
+const calculateCookingTime = orderQty => {
+  if (orderQty >= 1 && orderQty <= 2) {
     return 10;
   } else if (orderQty <= 5) {
     return 15;
-  } else {
+  } else if (orderQty >=6 && orderQty <= 50) {
     return 20;
+  } {
+    alert('Please enter a number between 1-50.')
   }
 };
 
 const order = () => {
   const orderName = prompt("Enter the name of the pizza you want to order today.");
   if (validateOrderName(orderName)) {
-    let orderQty = prompt(`How many of ${orderName} do you want?`);
-    let orderTotal = calculateTotalCost(orderQty, pizzaPrice);
-    let pizzaCookingTime = calculateCookingTime(orderQty);
+    const orderQty = prompt(`How many of ${orderName} do you want?`);
+    const orderTotal = calculateTotalCost(orderQty, pizzaPrice);
+    const pizzaCookingTime = calculateCookingTime(orderQty);
     document.getElementById("final-order").innerHTML = `Great! I will get started on your ${orderName} right away, it will cost ${orderTotal}kr. The pizza(s) will be ready in ${pizzaCookingTime} minutes.`;
     document.getElementById("button").style.display = "none";
     document.getElementById("menu").style.display = "none";
   } else {
     document.getElementById("menu").innerHTML =`Please select a pizza from our menu. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`;
   }
-}
-
-
-
-
-
-
-
-
-
-
+};
