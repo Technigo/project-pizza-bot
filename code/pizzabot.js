cb48f859ec930ed31af38d554e4b51da3424f5ad
Ingela Löfgren <ingela.lofgren@gmail.com>
2020-09-15 09:12:27 +0200

    Deleted commented code and fixed indentation in css

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -10,27 +10,6 @@ const pizzaPrice = 80
 console.log("Vegetarian Pizza", "Hawaiian Pizza","Pepperoni Pizza");
 
 
-
-//iteration 2// 
-//alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
-
-//iteration 3 ,4 and 5// 
-/* const orderName = prompt("Enter the name of the pizza you want to order today.");
-if ((orderName === vegetarian) || (orderName === hawaiian) || (orderName === pepperoni)) {
-  const orderQuantity = prompt(`How many ${orderName}(s) do you want?`);
-  const orderTotal = orderQuantity * pizzaPrice;
-    if (orderQuantity <= 2) {
-      alert(`Great, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take 10 minutes.`);
-    } else if (orderQuantity > 2 && orderQuantity <= 5) {
-      alert(`Great, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take 15 minutes.`);
-    } else {
-      alert(`Great, I'll get started on your ${orderName} pizza(s) right away, it will cost ${orderTotal} kr. The pizza(s) will take 20 minutes.`);
-    }
-} else {
-  alert('Select a pizza from the menu');
-}
-*/
-
 // prevent the alert to pop up. Must click "order pizza"
 
 const orderPizza = (event) => {
@@ -76,10 +55,6 @@ if (validateOrderName(orderName) === true) {
 
 }
 
-//Iteration 7 
-
-const calc = onsubmit
-document.getElementById("calc").innerHTML= (`Great I'll get started with your ${orderName}(s) right away, it will cost ${calculationTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes."`);
 
 
 
