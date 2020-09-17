42e8a7fe0b27db5283639f76d552160b9e3b1c16
JESSICA PANDITHA <JESSICA.PANDITHA@GMAIL.COM>
2020-09-17 17:10:49 +0200

    completed the points in the pull request

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -4,14 +4,11 @@ const pepperoni = "Pepperoni Pizza"
 
 const pizzaPrice = 80;
 
-//Put your Javscript code here:
-console.log(vegetarian,hawaiian,pepperoni);
 
+document.getElementById("welcome").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`;
 
-document.getElementById("welcome").innerHTML = `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
-
-const calculateCookingTime = (orderQuantity) => {
-    if (orderQuantity <= 2 ) {
+const calculateCookingTime = orderQuantity => {
+     if (orderQuantity <= 2) {
        return 10;
     } else if (orderQuantity >= 3 && orderQuantity <= 5) {
        return 15;
@@ -21,27 +18,32 @@ const calculateCookingTime = (orderQuantity) => {
 }
 
 
-const validateOrderName = (orderName) => {
+const validateOrderName = orderName => {
     if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni) {
-        return true 
-    } else { 
-        return false
-    }
-};
-
-const calculateTotalCost = (orderQuantity, pizzaPrice) => {
-    return orderQuantity*pizzaPrice;
+        return true;
+    }  
+        return false;
 }
 
-const handleSubmit = (event) => {
+const validateQuantity = orderQuantity => {
+    if (orderQuantity >= 1 && orderQuantity <=20) {
+        return true;
+    }  
+        return false;
+}
+
+const calculateTotalCost = (orderQuantity, pizzaPrice) => orderQuantity*pizzaPrice;
+
+const handleSubmit = event => {
     event.preventDefault();
-    const orderName = document.getElementById("pizzaName").value
-        /*prompt('Enter the name of the pizza you want to order today.');*/
+    const orderName = document.getElementById("pizzaName").value;
+          
     
-    validateOrderName(orderName); 
-    if (validateOrderName(orderName) === true) {
+    if (validateQuantity(document.getElementById("pizzaQuantity").value) === false) {
+        document.getElementById("orderConfirmation").innerHTML = `Choose a minimum of 1 pizza from the menu`;
+    }
+    else if (validateOrderName(orderName) === true && validateQuantity(document.getElementById("pizzaQuantity").value)=== true) {
         const orderQuantity = document.getElementById("pizzaQuantity").value;
-        /*prompt(`How many of ${orderName} do you want?`);*/
         const cookingTime = calculateCookingTime(orderQuantity);
         const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);  
         document.getElementById("orderConfirmation").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr and the pizzas will take ${cookingTime} minutes`; 
