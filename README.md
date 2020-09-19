3a592b6c646c2976cfda5ffa0db2b4bfcb049f24
Hanna Skeppe <hannaskeppe@gmail.com>
2020-09-19 20:25:15 +0200

    Made the requested changes after code-review.

--- README.md
+++ README.md
@@ -7,14 +7,16 @@ We completed the blue level and aimed at the red level.
 ## The problem
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? 
+
 We started coding the project in the beginning of the week when we hadn't had so much time to practice and learn Javascript basics. This proved to be a challange writing the code and simultaneously reading up on the material.
-We both had no prior knowledge of Javascript starting this project. Something we found helpful learning js basics was a Codecademy course that ran through the basics of Javascript, which we did parallell to the project.
-Because we har different life/work situations we didn't have the same amount of time to put in to the project. 
+We both had no prior knowledge of Javascript starting this project. Something we found helpful learning JS basics was a Codecademy course that ran through the basics of Javascript, which we did parallell to the project.
+Because we had different life/work situations we didn't have the same amount of time to put in to the project. 
 We think that the pair-programming worked really well, and we learned a lot from each other.
-Considering the above, we are in the end very pleased with how the end result turned out. But we feel that we have to continue to read up more on Javascript to be more comfortable in js basics. 
-For this project we din't focus a lot on the CSS styling, because we felt that ou time was best spent trying to grasp Javascript basics. We did add some styling and an animation though.
+Considering the above, we are in the end very pleased with how the end result turned out. But we feel that we have to continue to read up more on Javascript to be more comfortable in JS basics. 
+For this project we din't focus a lot on the CSS styling, because we felt that our time was best spent trying to grasp Javascript basics. We did add some styling and an animation though.
 
 If you had more time, what would be next?
+
 If we had more time, we would have also specified different prices to the pizzas. We find that making even small changes like this is quite complicated and takes much time, which we don't have right now.
 
 ## View it live
--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,28 +5,22 @@ const kebeb = "Kebab Pizza" //We added an extra pizza with the aim to specify a 
 
 const pizzaPrice = 80
 
-//console.log(vegetarian);
-//console.log(hawaiian);
-//console.log(pepperoni);
-
 /*Functions below */
 
 /*Check valid pizza name*/
-const validateOrderName = (orderName) =>{
+const validateOrderName = orderName =>{
     if (orderName === vegetarian || orderName === hawaiian || orderName === pepperoni || orderName === kebab)
     {
-        return true;
+      return true;
     }
-    else return false;
+    return false;
 }
-//console.log(validateOrderName);
 
 /*Calculate order total price*/
 const calculateTotalCost = (orderQuantity, pizzaPrice) =>{
    const orderTotal = orderQuantity * pizzaPrice;
    return orderTotal;
 }
-//console.log(calculateTotalCost);
 
 /*Calculate cooking time depending on orderQuantity*/
 const calculateCookingTime = (orderQuantity) =>{
@@ -42,7 +36,6 @@ const calculateCookingTime = (orderQuantity) =>{
     }
     return cookingTime;
 }
-//console.log(calculateCookingTime);
 
 /*Function to specify pizza image depending on pizza selection*/
 const pizzaImageFunction = (orderName) => {
@@ -60,21 +53,14 @@ const createPizzaOrder = () => {
     const customerName = document.getElementById('firstName').value;
     let orderName = document.getElementById('pizzaSelection').value;
     let orderQuantity = document.getElementById('pizzaRangeSlider').value;
-    //console.log(customerName);
-    //console.log(orderName);
-    //console.log(orderQuantity);
     
     if (customerName.length > 0) {
         const cookingTime = calculateCookingTime(orderQuantity);
-        //console.log(cookingTime);
         const orderTotal = calculateTotalCost(orderQuantity, pizzaPrice);
-        //console.log(orderTotal);
         const orderConfirmation = (`Hey, ${customerName}, I'll get started on your ${orderName} right away.
         It will cost ${orderTotal} kr. The pizza(s) will take ${cookingTime} minutes.`)
-        //console.log(orderConfirmation);
         document.getElementById('orderConfirmationText').innerHTML = orderConfirmation;
         pizzaPhoto.src = pizzaImageFunction(orderName); // Calls pizzaImage function to show the ordered Pizza's image by replacing src property
-        console.log(pizzaPhoto.src);
     } else {
         alert('Please specify all information in the form');
     }
@@ -91,7 +77,6 @@ const changeRangeValue = () => {
 
 //alert(`Hello! Happy to serve you pizza. On our menu we have ${vegetarian}, ${pepperoni}, ${hawaiian}`);
 const orderName = prompt("Enter the name of the pizza you want to order");
-console.log(orderName);
 
 if (validateOrderName(orderName)===true) {
     const orderQuantity = prompt(`How many of ${orderName} do you want?`);
