065ea3408bec5053582563aa6dc5b42c34eb7976
Marjaana Tienaho <marjaanatienaho@Marjaanas-Air.lan>
2020-09-21 20:07:11 +0200

    Fixed. Removed semicolons and removed commented code

--- pizzabot.js
+++ pizzabot.js
@@ -7,62 +7,62 @@ const kebab = "kebab"
 
 // Iteration 4 - Finalize the order
 const calculateTotalCost = (event) => {
-    event.preventDefault();
+    event.preventDefault()
     //Get nr of pizzas
-    let orderQuantity = document.getElementById("nrOfPizzas").value;
+    let orderQuantity = document.getElementById("nrOfPizzas").value
     //Get pizza sort
-    let orderName = document.getElementById("pizzaSort").value;
-    let pizzaPrice = pricePerPizza(orderName);
+    let orderName = document.getElementById("pizzaSort").value
+    let pizzaPrice = pricePerPizza(orderName)
     //Calculate totalcost for pizza
     const orderTotal = orderQuantity * pizzaPrice
     //Print out message to customer
     let orderTime = calculateCookingTime(orderQuantity)
     let pizzaImg = pizzaImages(orderName)
-    document.getElementById("pizzaImg").src = pizzaImg;
-    let info = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${orderTime} minutes.`;
+    document.getElementById("pizzaImg").src = pizzaImg
+    let info = `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${orderTime} minutes.`
     //Set pizza picture to display block
-    let x = document.getElementById("pizzaImg");
-    x.style.display = "block";
+    let x = document.getElementById("pizzaImg")
+    x.style.display = "block"
     //Display customer order confirmation
-    document.getElementById("submitInfo").innerHTML = info;
+    document.getElementById("submitInfo").innerHTML = info
 
-};
+}
 
-const form = document.getElementById("form");
-form.onsubmit = calculateTotalCost;
+const form = document.getElementById("form")
+form.onsubmit = calculateTotalCost
 
 //Sets the pizza image 
 const pizzaImages = (orderName) => {
 
     if (orderName == vegetarian) {
-        return "./Vegetariana.jpg";
+        return "./Vegetariana.jpg"
     } else if (orderName == hawaiian) {
-        return "./Hawaiian.jpg";
+        return "./Hawaiian.jpg"
     } else if (orderName == pepperoni) {
-        return "./Pepperoni.jpg";
+        return "./Pepperoni.jpg"
     } else if (orderName == calzone) {
-        return "./Calzone.jpg";
+        return "./Calzone.jpg"
     } else if (orderName == kebab) {
-        return "./Kebab.jpg";
+        return "./Kebab.jpg"
     }
 
 }
 //Sets the pizza price
 const pricePerPizza = (orderName) => {
-    let pizzaPrices = 0;
+    let pizzaPrices = 0
     if ((orderName == `${vegetarian}`) || (orderName == `${hawaiian}`) || (orderName == `${pepperoni}`)) {
         pizzaPrices = 80
-        return pizzaPrices;
+        return pizzaPrices
 
     } else {
         pizzaPrices = 100
-        return pizzaPrices;
+        return pizzaPrices
     }
 
 }
 //Add conditonals (Iteration 5)
 const calculateCookingTime = (orderQuantity) => {
-    let orderTime = 0;
+    let orderTime = 0
     if (orderQuantity <= 2) {
         orderTime = 10
     } else if (orderQuantity >= 3 && orderQuantity <= 5) {
@@ -71,44 +71,8 @@ const calculateCookingTime = (orderQuantity) => {
     } else if (orderQuantity >= 6) {
         orderTime = 20
     }
-    return orderTime;
+    return orderTime
 }
 
 
 
-//Below code is from the itterations in blue level
-/*
-const pizzaPrice = 80
-
-//Greet the customer (Iteration 2)
-alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}.`)
-
-//Ask the customer (Iteration 3)
-let orderTotal
-let orderTime
-let orderName = prompt("Enter the name of the pizza you want to order today: ")
-let validation = validateOrderName(orderName);
-if (validation === true) {
-    const orderQuantity = prompt(`How many of ${orderName} do you want?`)
-    console.log(orderQuantity)
-    orderTotal= calculateTotalCost(orderQuantity, pizzaPrice)
-    orderTime=calculateCookingTime(orderQuantity)
-} else {
-    orderName = prompt("Enter the name of the pizza you want to order today: ")
-    console.log(orderName)
-    validateOrderName(orderName)
-}
-
-/*const validateOrderName = (orderName) => {
-   
-    console.log(orderName)
-    if ((orderName == `${vegetarian}`) || (orderName == `${hawaiian}`) || (orderName == `${pepperoni}`)) {
-        return true;
-
-    } else {
-        alert("Select a pizza from the menu")
-        return false;
-    }
-}*/
-
-//alert(`Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizza(s) will take ${orderTime} minutes.`)
