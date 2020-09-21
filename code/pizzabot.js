48c39f30d4e25d5b06493bc154e956716d3b9eb7
Jamie Cook <jamie.p.cook@outlook.com>
2020-09-21 10:07:10 +0200

    cleaned up code

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -10,60 +10,61 @@ let orderName;
 let cookingTime;
 let imageName;
 
-  const askForPizza = (event) => {
-    event.preventDefault();
-    alert(`Welcome to Pizza Corner! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`); 
+const askForPizza = () => {
+  orderName = prompt("Enter the name of the pizza you want to order today");
+  validateOrderName(orderName);
+};
 
-  const validateOrderName = (order) => {
-    if (
-      order === "vegetarian pizza" ||
-      order === "pepperoni pizza" ||
-      order === "hawaiian pizza" 
-    ) {
-      orderQuantity = prompt(`How many ${orderName}'s would you like?`);
-      orderQuantity = parseInt(orderQuantity, 10);
-    } else {
-      alert("Please choose a pizza from our menu");
-      askForPizza();
-    }
-  };
+const calculateTotalCost = (quantity, price) => {
+  orderTotal = quantity * price;
+};
 
-  const askForPizza = () => {
-    //alert(`Welcome to Pizza Corner! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`); 
-    orderName = prompt("Enter the name of the pizza you want to order today"); 
-    validateOrderName(orderName);
-  };
+const finalizeOrder = (order) => {
+  if (order <= 2) {
+    cookingTime = 10;
+  } else if (order > 2 && order < 6) {
+    cookingTime = 15;
+  } else {
+    cookingTime = 20;
+  }
+  alert(
+    `Great I'll get started on your ${orderName} order right away. It will cost ${orderTotal}kr. The pizza(s) will take ${cookingTime} minutes`
+  );
 
+  document.getElementById(
+    "printOrder"
+  ).innerHTML = `You have ordered: ${orderName} x${orderQuantity} and the total price is: ${orderTotal}kr`;
+  document.getElementById(
+    "pizzaImage"
+  ).innerHTML = pizzaImage.src = findPizzaImage(orderName);
+};
+
+const findPizzaImage = (order) => {
+  if (order === vegetarian) {
+    return "./images/vegetarian-pizza.jpg";
+  } else if (order === pepperoni) {
+    return "./images/pepperoni-pizza.jpg";
+  } else {
+    return "./images/hawaiian-pizza.jpg";
+  }
+};
+
+const validateOrderName = (order) => {
+  if (order === vegetarian || order === pepperoni || order === hawaiian) {
+    orderQuantity = prompt(`How many ${orderName}'s would you like?`);
+    orderQuantity = parseInt(orderQuantity, 10);
+    calculateTotalCost(orderQuantity, pizzaPrice);
+    finalizeOrder(orderQuantity);
+  } else {
+    alert("Please choose a pizza from our menu");
+    askForPizza();
+  }
+};
+
+const welcomeMessage = (event) => {
+  event.preventDefault();
+  alert(
+    `Welcome to Pizza Corner! On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+  );
   askForPizza();
-  const calculateTotalCost = (quantity, price) => {
-    orderTotal = quantity * price;
-  };
-
-  const pizzaImgFunction = (order) => {
-    if(order === "vegetarian pizza") {
-        return "./images/vegetarian-pizza.jpg";
-    } else if (order === "pepperoni pizza") {
-        return "./images/pepperoni-pizza.jpg";
-    } else {
-        return "./images/hawaiian-pizza.jpg";
-    }
-  };
-
-  const calculateCookingTime = (order) => {
-    if (order <= 2) {
-      cookingTime = 10;
-    } else if (order > 2 && order < 6) {
-      cookingTime = 15;
-    } else {
-      cookingTime = 20;
-    }
-    alert(`Great I'll get started on your ${orderName} order right away. It will cost ${orderTotal}kr. The pizza(s) will take ${cookingTime} minutes`);
-
-    document.getElementById("printOrder").innerHTML = `You have ordered: ${orderName} x${orderQuantity} and the total price is: ${orderTotal}kr`;
-    document.getElementById("pizzaImage").innerHTML = pizzaImage.src = pizzaImgFunction(orderName);
-
-  };
-
-  calculateTotalCost(orderQuantity, pizzaPrice);
-  calculateCookingTime(orderQuantity);
 };
