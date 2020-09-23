ec8962902214a5fe180483cfd0fa1ff3e8d10c8c
Sara Sundblom <sundblomsara@gmail.com>
2020-09-23 17:11:27 +0200

    code review changes

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,24 +1,24 @@
-const vegetarian = "Vegetarian Pizza";
-const hawaiian = "Hawaiian Pizza";
-const pepperoni = "Pepperoni Pizza";
+const vegetarian = "Vegetarian Pizza"
+const hawaiian = "Hawaiian Pizza"
+const pepperoni = "Pepperoni Pizza"
 
-const pizzaPrice = 80;
+const pizzaPrice = 80
 
-let botSays = document.getElementById('botSays');
-botSays.innerHTML = 'What would you like to order?';
+const botSays = document.getElementById('botSays')
+botSays.innerHTML = 'What would you like to order?'
 
 // make inputbox actice (the mouse/cursor is there in beginning)
-document.getElementById("answer").focus();
+document.getElementById("answer").focus()
 
 // used to check if valid pizza is ordered
-let orderedPizza = "";
+let orderedPizza = ""
 
 const isValidPizza = (pizza) => {
     if (vegetarian.toLowerCase() === pizza.toLowerCase() ||
         hawaiian.toLowerCase() === pizza.toLowerCase() ||
         pepperoni.toLowerCase() === pizza.toLowerCase()) {
         return true
-    } else {
+    } else { 
         return false
     }
 }
@@ -41,8 +41,8 @@ const calculateCookingTime = (orderedQuantity) => {
 const submitAnswer = () => {
 
     // always keep the inputbox active
-    document.getElementById("answer").focus();
-    let answer = document.getElementById("answer").value;
+    document.getElementById("answer").focus()
+    let answer = document.getElementById("answer").value
 
     // If no pizza, or no valid pizza, has been ordered..
     if (orderedPizza === "") {
@@ -51,39 +51,39 @@ const submitAnswer = () => {
         let validPizza = isValidPizza(answer)
 
         if (validPizza) {
-            orderedPizza = answer;
+            orderedPizza = answer
             document.getElementById("answer").placeholder = `cool, tell me how many.`
-            botSays.innerHTML = `How many ${orderedPizza} would you like?`;
+            botSays.innerHTML = `How many ${orderedPizza} would you like?`
         } else {
-            botSays.innerHTML = `Sorry, we don't serve ${answer}. Please order something from the menu`;
+            botSays.innerHTML = `Sorry, we don't serve ${answer}. Please order something from the menu`
         }
 
-        document.getElementById("answer").value = "";
+        document.getElementById("answer").value = ""
     } else {
         // GET ORDER QUANTITY
         let orderQuantity = answer
 
         // CALCULATE COST
-        let totalCost = calculateTotalCost(orderQuantity, pizzaPrice);
+        let totalCost = calculateTotalCost(orderQuantity, pizzaPrice)
 
         // CALCULATE COOKING TIME
-        let cookingTime = calculateCookingTime(orderQuantity);
+        let cookingTime = calculateCookingTime(orderQuantity)
 
         botSays.innerHTML = `Nice! ${orderQuantity} ${orderedPizza}'s will cost you ${totalCost}kr
-                             and take ${cookingTime} minutes. `;
+                             and take ${cookingTime} minutes. `
 
         // Reset order mode for next customer
-        document.getElementById("answer").value = "";
-        orderedPizza = "";
+        document.getElementById("answer").value = ""
+        orderedPizza = ""
         document.getElementById("answer").placeholder = `enter pizza name`
     }
 }
 
-let input = document.getElementById("answer");
+let input = document.getElementById("answer")
 input.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("submitButton").click();
+        event.preventDefault()
+        document.getElementById("submitButton").click()
     }
-});
+})
 
