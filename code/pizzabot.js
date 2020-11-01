32953ac2d1550b938de240908a8f8025aff8c6cf
caroline birgersson <caroline@birgerochblom.se>
2020-11-01 20:39:05 +0100

    changed indentation after change request

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,40 +1,44 @@
 ////JAVASCRIPT////
-const vegetarian = "Vegetarian Pizza"
-const hawaiian = "Hawaiian Pizza"
-const pepperoni = "Pepperoni Pizza"
+const vegetarian = "Vegetarian Pizza";
+const hawaiian = "Hawaiian Pizza";
+const pepperoni = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+const pizzaPrice = 80;
 
 //Greeting:
 
-const x = "vegetarian"
-const y = "hawaiian"
-const z = "pepperoni"
+const x = "vegetarian";
+const y = "hawaiian";
+const z = "pepperoni";
 
-
-document.getElementById("greeting").innerHTML = (`Hey! Happy to serve you pizza! On the menu we have ${x}, ${y} and ${z}.`)
-
+document.getElementById(
+  "greeting"
+).innerHTML = `Hey! Happy to serve you pizza! On the menu we have ${x}, ${y} and ${z}.`;
 
 const calculateTotalCost = (orderQuantity) => {
-  orderTotal = (orderQuantity * pizzaPrice)
-}
+  orderTotal = orderQuantity * pizzaPrice;
+};
 
 const calculateCookingTime = (orderQuantity) => {
   if (orderQuantity > 0 && orderQuantity <= 2) {
-      cookingTime = 10
+    cookingTime = 10;
   } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-      cookingTime = 15
+    cookingTime = 15;
   } else if (orderQuantity >= 6) {
-      cookingTime = 20
+    cookingTime = 20;
   }
-}
+};
 
 const validateOrderName = (orderName, orderQuantity) => {
   if (orderName === x || orderName === y || orderName === z) {
-      calculateTotalCost(orderQuantity)
-      calculateCookingTime(orderQuantity)
-      document.getElementById("final").innerText = (`Thanks ${userName.value}! You just ordered ${orderQuantity} ${orderName}. The total cost is ${orderTotal} kr and the order will take ${cookingTime} minutes.`)
+    calculateTotalCost(orderQuantity);
+    calculateCookingTime(orderQuantity);
+    document.getElementById(
+      "final"
+    ).innerText = `Thanks ${userName.value}! You just ordered ${orderQuantity} ${orderName}. The total cost is ${orderTotal} kr and the order will take ${cookingTime} minutes.`;
   } else {
-      document.getElementById("final").innerText = (`Sorry, that pizza isn't on the menu. Please choose between ${x}, ${y} or ${z}.`)
+    document.getElementById(
+      "final"
+    ).innerText = `Sorry, that pizza isn't on the menu. Please choose between ${x}, ${y} or ${z}.`;
   }
-  }
+};
